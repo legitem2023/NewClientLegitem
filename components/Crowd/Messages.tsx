b55480de347dff2f895d94c41ed5e66df4211a11
Robert <legitem2023@gmail.com>
2025-02-08 16:16:17 +0800

    Streaming Live 3

--- components/Crowd/Messages.tsx
+++ components/Crowd/Messages.tsx
@@ -128,9 +128,9 @@ console.log(streaming,"<<<<")
           {streaming !==''?
          <div className='messagesLI'> 
           <video width="320" height="240" src={streaming || null} className='messageVideo'  controls>
-          <source src="movie.mp4" type="video/mp4"/>
-          <source src="movie.ogg" type="video/ogg"/>
-          </video>
+          <source src={streaming || null}/>
+          <source src={streaming || null}/>
+         </video>
           </div>
           :<></>}
         </>
