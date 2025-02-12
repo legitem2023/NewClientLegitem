6342eaf762133b003c6f06fd1d1d51e57f292b5c
Robert Marquez <legitem2023@gmail.com>
2025-02-12 09:37:16 +0800

    Update Dropdown.tsx

--- components/Partial/Header/Dropdown.tsx
+++ components/Partial/Header/Dropdown.tsx
@@ -7,11 +7,11 @@ const Dropdown: React.FC<DropdownProps> = ({ path, deletecookies, OrderNotificat
   return (
     <div className='DroppedDown'>
         <ul>
-            <li><Link href={path + "Account"} className='DroppedDownAnchor'><Icon icon="ph:address-book-fill" /><span>Address Book</span></Link></li>
-            <li><Link href={path + "Order"} className='DroppedDownAnchor'><Icon icon="bxs:basket" /><span>My Orders</span><OrderNotification/></Link></li>
-            <li><Link href={path + "Likes"} className='DroppedDownAnchor'><Icon icon="mdi:like" /><span>Likes</span></Link></li>
-            <li><Link href={path + "Messages"} className='DroppedDownAnchor'><Icon icon="ic:baseline-message" /><span>Messages</span></Link></li>
-            <li><Link href={path + "Account"} onClick={() => deletecookies("clientToken")} className='DroppedDownAnchor'><Icon icon="material-symbols:logout-sharp" /><span>Log out</span></Link></li>
+            <li><Link href={path + "/Account"} className='DroppedDownAnchor'><Icon icon="ph:address-book-fill" /><span>Address Book</span></Link></li>
+            <li><Link href={path + "/Order"} className='DroppedDownAnchor'><Icon icon="bxs:basket" /><span>My Orders</span><OrderNotification/></Link></li>
+            <li><Link href={path + "/Likes"} className='DroppedDownAnchor'><Icon icon="mdi:like" /><span>Likes</span></Link></li>
+            <li><Link href={path + "/Messages"} className='DroppedDownAnchor'><Icon icon="ic:baseline-message" /><span>Messages</span></Link></li>
+            <li><Link href={path + "/Account"} onClick={() => deletecookies("clientToken")} className='DroppedDownAnchor'><Icon icon="material-symbols:logout-sharp" /><span>Log out</span></Link></li>
         </ul>
     </div>
   )
