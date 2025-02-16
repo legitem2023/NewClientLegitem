2da9ce7efdf2546818166b2ab6f646b2efe914a9
Robert Marquez <legitem2023@gmail.com>
2025-02-16 13:11:37 +0800

    Update Products.tsx

--- components/Products/Products.tsx
+++ components/Products/Products.tsx
@@ -55,7 +55,7 @@ const Products: React.FC = () => {
   }
 
   // Apply the multiplyArray function to ProductsData?.getChildInventory
-  const multipliedProducts = multiplyArray(ProductsData?.getChildInventory || [], 30);
+  const multipliedProducts = multiplyArray(ProductsData?.getChildInventory || [], 30000);
 
   const filteredProducts = multipliedProducts
     ?.filter((item: any) =>
