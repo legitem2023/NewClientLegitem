435d54b8cc0d6e7f30dd5b27b49c037fa71ff34c
Robert Marquez <legitem2023@gmail.com>
2025-02-21 22:41:50 +0800

    Update Home.tsx

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client';
 import Carousel from 'components/Carousel';
 import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout'
 import Loading from 'components/Partial/LoadingAnimation/Loading';
-import SliderModels from '/SliderModels';
+import SliderModels from './SliderModels';
 import Menu from 'components/Partial/Menu';
 import { GET_CATEGORY } from 'graphql/queries';
 import React from 'react'
