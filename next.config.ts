a095e5576b3d9958492e7a0c11043a3d04584bed
Robert Marquez <legitem2023@gmail.com>
2025-02-22 09:43:44 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['readymadeui.com','picsum.photos','localhost','tsbriguuaznlvwbnylop.supabase.co','192.168.100.86','cdn-icons-png.flaticon.com','hokei-storage.s3.ap-northeast-1.amazonaws.com','client-legitem.vercel.app'],
+        domains: ['readymadeui.com','picsum.photos','localhost','tsbriguuaznlvwbnylop.supabase.co','192.168.100.86','cdn-icons-png.flaticon.com','hokei-storage.s3.ap-northeast-1.amazonaws.com','client-legitem.vercel.app','modelviewer.dev'],
       },
 }
 
