5571e9c9e0ffc5566d2ff91e9346cf34642f0a2b
Robert Marquez <legitem2023@gmail.com>
2025-02-23 00:18:39 +0800

    Update RecentlyVisited.tsx

--- components/Products/RecentlyVisited.tsx
+++ components/Products/RecentlyVisited.tsx
@@ -1,1 +1,14 @@
+"use client";
 
+import { useEffect,useState } from "react";
+
+const RecentlyVisited = () => {
+const [visited,setVisited] = useState([]);
+useEffect(()=>{
+ const localstorage = JSON.parse(localStorage.getItem('recentlyVisited'));
+   setVisited(localstorage)
+})
+  return <div>{/* Product details here */}</div>;
+};
+
+export default RecentlyVisited;
