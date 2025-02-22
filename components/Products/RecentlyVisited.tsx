9cafdf055f8d93582ed1ee94a067232341c26748
Robert Marquez <legitem2023@gmail.com>
2025-02-23 01:11:27 +0800

    Update RecentlyVisited.tsx

--- components/Products/RecentlyVisited.tsx
+++ components/Products/RecentlyVisited.tsx
@@ -15,7 +15,7 @@ const [visited,setVisited] = useState([]);
 useEffect(()=>{
  const localstorage = JSON.parse(localStorage.getItem('recentlyVisited'));
    setVisited(localstorage)
-})
+},[localStorage])
  const path = process.env.NEXT_PUBLIC_PATH || '';
    const openModal = (id: string, items: any) => {
     dispatch(setviewed(id));
