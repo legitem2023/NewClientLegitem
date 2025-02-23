de033d8b5d6b27355b3ebba16bf1938a47194367
Robert Marquez <legitem2023@gmail.com>
2025-02-23 19:20:53 +0800

    Update ClientIP.tsx

--- components/Commands/ClientIP.tsx
+++ components/Commands/ClientIP.tsx
@@ -4,7 +4,7 @@ export default function ClientIP() {
   const [ip, setIp] = useState("Loading...");
 
   useEffect(() => {
-    fetch("https://api64.ipify.org?format=json")
+    fetch("https://ipinfo.io/json")
       .then(res => res.json())
       .then(data => setIp(data.ip))
       .catch(() => setIp("Failed to fetch IP"));
