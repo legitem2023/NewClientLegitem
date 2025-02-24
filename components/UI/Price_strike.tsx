2e0de14d69c9bb23563f041f010d8eec55af1cf1
Robert Marquez <legitem2023@gmail.com>
2025-02-24 17:50:58 +0800

    Update Price_strike.tsx

--- components/UI/Price_strike.tsx
+++ components/UI/Price_strike.tsx
@@ -7,7 +7,7 @@ type PropsPrice = {
 const Price_strike:React.FC<PropsPrice> = ({item}) => {
   return (
   <div>
-        {item.discount > 0?(<span className='thumbElements_tag'>Less {100 - (item.discount * 100)}%</span>):""}        
+     /*{item.discount > 0?(<span className='thumbElements_tag'>Less {100 - (item.discount * 100)}%</span>):""} */  
         <s className='thumbElements'>{formatter.format(item.price)}</s>
   </div>
   )
