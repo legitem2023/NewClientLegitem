39a245d4b745d25464fe65c9332af402f7b0ee93
Robert Marquez <legitem2023@gmail.com>
2025-02-24 14:18:44 +0800

    Update ActiveUsers.tsx

--- components/PersonalMessages/ActiveUsers.tsx
+++ components/PersonalMessages/ActiveUsers.tsx
@@ -28,12 +28,7 @@ const ActiveUsers = ({email}) => {
                                                           width:'100%',
                                                           boxSizing:'border-box'}}/></li>
     <li className='Menu_label'>Conversations</li>
-      {[...new Set(data.personalMessages
-  .filter((itm: any) => itm.Sender !== cookie.emailAddress)
-  .map((item: any) => item.Sender)
-)].map((sender, idx) => (
-  <li key={idx}>{sender}</li>
-))}
+      {[...new Set(data.personalMessages.filter((itm: any) => itm.Sender !== cookie.emailAddress).map((item: any) => item.Sender))].map((sender, idx) => (<li key={idx}>{sender.Sender}</li>))}
     
     </ul> )
 }
