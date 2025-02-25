e69cb50a8a48974367e8fbeae9db422203dad43f
Robert Marquez <legitem2023@gmail.com>
2025-02-25 15:29:39 +0800

    Update CrowdMessages.tsx

--- components/Crowd/CrowdMessages.tsx
+++ components/Crowd/CrowdMessages.tsx
@@ -4,13 +4,13 @@ import Messages from './Messages'
 import { Icon } from '@iconify/react'
 import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
 import { useSelector } from 'react-redux'
-
+import ActiveUsers from 'components/PersonalMessages/ActiveUsers';
 const CrowdMessages = () => {
   const cookie = useSelector((state:any)=> state.cookie.cookie);
     return (
         <ReusableMainLayout
             childA={()=>(
-                <></>
+                <ActiveUsers email={cookie.emailAddress}/>
             )}
             childB={()=>(
                 <Messages />
