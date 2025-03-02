dd70bc1c2e41fe2aa5f921898ee0822d49fc4986
Robert Marquez <legitem2023@gmail.com>
2025-03-02 17:04:34 +0800

    Update ReusableMessage.tsx

--- components/UI/ReusableMessage.tsx
+++ components/UI/ReusableMessage.tsx
@@ -3,6 +3,7 @@ import ExpandableText from 'components/Crowd/ExpandableText';
 import Image from 'next/image';
 import React, { FC, useEffect, useState } from 'react';
 import ReusableFirstLetterImage from './ReusableFirstLetterImage';
+import ReusableDropdownProps from './ReusableDropdownProps';
 import LiveStreamPlayer from './LiveStreamPlayer';
 import { useSelector } from 'react-redux';
 
@@ -69,7 +70,10 @@ const ReusableMessage: FC<ReusableMessageProps> = ({ data, onChange }) => {
           <div className={`messageSenderImgcont ` + border(data)}>
             <ReusableFirstLetterImage text={data.Sender} size={100} bgColor="rgb(87, 39, 0)" textColor="#ffffff" />
           </div>
-          <b className="messageSenderName">{data.Sender}</b>
+          <div className="messageSenderName">
+            <ReusableDropdownProps Name={data.Sender} child1={()=>(<button></button>)} child1={()=>(<></>)}/>
+            {data.Sender}
+          </div>
           <div className="messageSenderTime">
             <Icon icon="svg-spinners:clock" width="15" height="15" style={{ marginLeft: '5px' }} />
             {noOfDays(data.dateSent)}
