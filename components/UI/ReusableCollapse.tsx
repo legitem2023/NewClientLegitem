78c2794dfa76f9680fc70f948aaba2f520cd3ad1
Robert Marquez <legitem2023@gmail.com>
2025-03-03 13:54:33 +0800

    Update ReusableCollapse.tsx

--- components/UI/ReusableCollapse.tsx
+++ components/UI/ReusableCollapse.tsx
@@ -22,7 +22,7 @@ const ReusableCollapse:React.FC<ReusableCollapseProps> = ({NameIcon,child1}) => 
   }, [handleClickOutside]);
   
   return (
-    <div ref={dropdownRef} style={{display:'flex',position:"absolute",flexDirection:'column',alignItems:'flex-end'}}className="relative inline-block text-left">
+    <div ref={dropdownRef} style={{display:'flex',position:"relative",flexDirection:'column',alignItems:'flex-end'}}className="relative inline-block text-left">
       {/* Three Dots Button */}
       <div
         onClick={() => setIsOpen(!isOpen)}
@@ -39,8 +39,9 @@ const ReusableCollapse:React.FC<ReusableCollapseProps> = ({NameIcon,child1}) => 
 
       {/* Dropdown Menu */}
       {isOpen && (
-        <div  style={{position: "relative",
+        <div  style={{position: "absolute",
                       width: "auto", 
+                      marginTop:'100%',
                       backgroundColor: "white",
                       border: "1px solid #E5E7EB", // gray-200
                       padding:'3px', // rounded-md
