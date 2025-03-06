9a14c5e5e49579dcb132c1c42d0251c96918d16b
Robert Marquez <legitem2023@gmail.com>
2025-03-07 00:10:29 +0800

    Update TypingIndicator.tsx

--- components/UI/TypingIndicator.tsx
+++ components/UI/TypingIndicator.tsx
@@ -1,23 +1,10 @@
-import { gql, useSubscription } from "@apollo/client";
+import React from "react";
+import useUserTypingSubscription from "./useUserTypingSubscription";
 
-const USER_TYPING_SUBSCRIPTION = gql`
-  subscription UserTyping($senderEmail: String!, $receiverEmail: String!) {
-    userTyping(senderEmail: $senderEmail, receiverEmail: $receiverEmail) {
-      senderEmail
-      receiverEmail
-      isTyping
-    }
-  }
-`;
+const TypingIndicator = ({ senderEmail, receiverEmail }: { senderEmail: string; receiverEmail: string }) => {
+  const typingData = useUserTypingSubscription(senderEmail, receiverEmail);
 
-const TypingIndicator = ({ senderEmail, receiverEmail }) => {
-  const { data } = useSubscription(USER_TYPING_SUBSCRIPTION, {
-    variables: { senderEmail, receiverEmail }
-  });
-
-  return data?.userTyping?.isTyping ? (
-    <p>{data.userTyping.senderEmail} is typing...</p>
-  ) : null;
+  return typingData?.isTyping ? <p>{typingData.senderEmail} is typing...</p> : null;
 };
 
-export default TypingIndicator
+export default TypingIndicator;
