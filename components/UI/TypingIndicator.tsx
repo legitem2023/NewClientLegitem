947fa96e7d383400e56030bb859bb32cb18c268b
Robert Marquez <legitem2023@gmail.com>
2025-03-07 01:36:02 +0800

    Update TypingIndicator.tsx

--- components/UI/TypingIndicator.tsx
+++ components/UI/TypingIndicator.tsx
@@ -3,7 +3,7 @@ import useUserTypingSubscription from "./useUserTypingSubscription";
 
 const TypingIndicator = ({ senderEmail, receiverEmail }: { senderEmail: string; receiverEmail: string }) => {
   const typingData = useUserTypingSubscription(senderEmail, receiverEmail);
-
+  console.log(typingData?.isTyping,"<---");
   return typingData?.isTyping ? <p>{typingData.senderEmail} is typing...</p> : null;
 };
 
