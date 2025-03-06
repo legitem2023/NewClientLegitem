799dcc1cc42a3f98ae72d2c003bebccaa79e3b24
Robert Marquez <legitem2023@gmail.com>
2025-03-07 02:02:03 +0800

    Update TypingIndicator.tsx

--- components/UI/TypingIndicator.tsx
+++ components/UI/TypingIndicator.tsx
@@ -4,8 +4,9 @@ import useUserTypingSubscription from "./useUserTypingSubscription";
 
 const TypingIndicator = ({ senderEmail, receiverEmail }: { senderEmail: string; receiverEmail: string }) => {
   const typingData = useUserTypingSubscription(senderEmail, receiverEmail);
-  console.log(typingData?.isTyping,"<---");
-  return typingData?.isTyping ? <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center'}}>{typingData.receiverEmail} is typing <Icon icon="svg-spinners:3-dots-bounce" width="24" height="24" /> </div> : null;
+  return typingData?.isTyping ? <div style={{display:'flex',justifyContent:'flex-start',alignItems:'center',backgroundColor:'#f1f1f1',padding:'3px'}}>
+    
+    {typingData.receiverEmail} is typing <Icon icon="svg-spinners:3-dots-bounce" width="24" height="24" /> </div> : null;
 };
 
 export default TypingIndicator;
