c07a01de61b32c61e84b29d7389a32e0c08a0316
Robert Marquez <legitem2023@gmail.com>
2025-03-19 17:38:49 +0800

    Update Products.tsx

--- components/Products/Products.tsx
+++ components/Products/Products.tsx
@@ -202,7 +202,7 @@ const sampleData = [
         <div
           style={{ overflowY: 'auto', height: 'auto', scrollbarWidth: 'none' }} // Set height to auto
         >
-          <RecentlyVisited data={sampleData} fromData={false}/>
+          <RecentlyVisited data={useSaved} fromData={false}/>
           <ReusableLabel icn='bi:tags-fill' label='Products'/>
           <div className="Thumbnails">
             {visibleProducts.length > 0 ? (
