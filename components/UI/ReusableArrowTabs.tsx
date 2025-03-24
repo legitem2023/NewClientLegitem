465b6dc0eac2ff29225421dd3d4fcb592cb07d79
Robert Marquez <legitem2023@gmail.com>
2025-03-24 19:52:35 +0800

    Update ReusableArrowTabs.tsx

--- components/UI/ReusableArrowTabs.tsx
+++ components/UI/ReusableArrowTabs.tsx
@@ -61,7 +61,10 @@ const ReusableArrowTabs: React.FC<TabsProps> = ({ tabs }) => {
               }}
             >
               <Icon icon={tab.icon} style={{ fontSize: "18px" }} />
-<ReusableNotification number={tabs[activeIndex]?.notification}/>
+{/* Adjust Notification Position */}
+  <div style={{ position: "absolute", top: "-10px", right: "0px", zIndex: 10 }}>
+    <ReusableNotification number={tab.notification} />
+  </div>
             </div>
           );
         })}
