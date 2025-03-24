94065bde5d0f02a0d56f734b62a5bbfb8d665178
Robert Marquez <legitem2023@gmail.com>
2025-03-25 07:43:58 +0800

    Update ReusableNotification.tsx

--- components/UI/ReusableNotification.tsx
+++ components/UI/ReusableNotification.tsx
@@ -5,7 +5,7 @@ const ReusableNotification = ({ number }) => {
   return (
     <div className='ReusableNotification'>
       {/* Bell Icon with SVG Border */}
-      <svg width="50" height="50" viewBox="0 0 50 50">
+      <svg width="31" height="31" viewBox="0 0 50 50">
         <path
           d="M25 5C15 5 5 15 5 25C5 35 0 40 0 45H50C50 40 45 35 45 25C45 15 35 5 25 5Z"
           stroke="black"
