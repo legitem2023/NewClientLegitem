21a81c6e4376c34b3beecf80a486efbf6070c020
Robert Marquez <legitem2023@gmail.com>
2025-03-29 14:56:04 +0800

    Update ReusableNotification.tsx

--- components/UI/ReusableNotification.tsx
+++ components/UI/ReusableNotification.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ReusableNotification = ({number}) => {
   return (
-    <div className='ReusableNotification'>
+    <div className='ReusableNotification' style={{transform:number>0?scale(parseInt(1):scale(0))}}>
       <div className="icon-container">
         <Icon icon="mdi:bell" width="20" height="20" className="icon-with-border"/>
       </div>
