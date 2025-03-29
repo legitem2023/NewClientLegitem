e9171088a4ed9c28d56a5b91283749a8bc9511f7
Robert Marquez <legitem2023@gmail.com>
2025-03-29 15:01:51 +0800

    Update ReusableNotification.tsx

--- components/UI/ReusableNotification.tsx
+++ components/UI/ReusableNotification.tsx
@@ -1,17 +1,22 @@
-import { Icon } from '@iconify/react'
-import React from 'react'
+import { Icon } from "@iconify/react";
+import React from "react";
 
-const ReusableNotification = ({number}) => {
+type PropsReusableNotification = {
+  number: number;
+};
+
+const ReusableNotification: React.FC<PropsReusableNotification> = ({ number }) => {
   return (
-    <div className='ReusableNotification' style={{transform:number>0?scale(parseInt(1):scale(0))}}>
+    <div
+      className="ReusableNotification"
+      style={{ transform: `scale(${number > 0 ? 1 : 0})` }}
+    >
       <div className="icon-container">
-        <Icon icon="mdi:bell" width="20" height="20" className="icon-with-border"/>
+        <Icon icon="mdi:bell" width="20" height="20" className="icon-with-border" />
       </div>
-      <div className="countnumber">
-        {number}
-      </div>
+      <div className="countnumber">{number}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ReusableNotification
+export default ReusableNotification;
