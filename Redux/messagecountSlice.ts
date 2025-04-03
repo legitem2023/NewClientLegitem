98ac0454eed4ccffe16c62e79a3d3d1be04e1180
Robert Marquez <legitem2023@gmail.com>
2025-04-03 16:35:06 +0800

    Update messagecountSlice.ts

--- Redux/messagecountSlice.ts
+++ Redux/messagecountSlice.ts
@@ -1,7 +1,8 @@
 'use client'
 import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = {
-  messagecount: 0, // Initial state for messagecount text
+  messagecount: typeof window !== 'undefined' ? Number(localStorage.getItem('messagecount')) || 0 : 0,
 };
 
 const messagecountSlice = createSlice({
@@ -9,14 +10,19 @@ const messagecountSlice = createSlice({
   initialState,
   reducers: {
     setmessagecount: (state, action) => {
-      state.messagecount = action.payload; // Update messagecount text
+      state.messagecount = action.payload;
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('messagecount', action.payload);
+      }
     },
     resetmessagecount: (state) => {
-      state.messagecount = 0; // Reset messagecount text to an empty string
+      state.messagecount = 0;
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('messagecount', '0');
+      }
     },
   },
 });
-
 
 export const { setmessagecount, resetmessagecount } = messagecountSlice.actions;
 export default messagecountSlice.reducer;
