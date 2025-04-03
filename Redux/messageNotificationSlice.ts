59608edff1574ba96fd0408951c2a333acfdb58a
Robert Marquez <legitem2023@gmail.com>
2025-04-03 21:11:48 +0800

    Update messageNotificationSlice.ts

--- Redux/messageNotificationSlice.ts
+++ Redux/messageNotificationSlice.ts
@@ -8,7 +8,21 @@ const localStorageKey = 'messageNotification';
 
 const initialState: MessageNotificationState = loadState();
 
-const messageNotificationSlice = createSlice({ name: 'messageNotification', initialState, reducers: { setMessageNotification: ( state, action: PayloadAction<{ message: string; email: string }> ) => { state.message = action.payload.message; state.email = action.payload.email; localStorage.setItem(localStorageKey, JSON.stringify(state)); }, clearMessageNotification: (state) => { state.message = ''; state.email = ''; localStorage.removeItem(localStorageKey); }, }, });
+const messageNotificationSlice = createSlice({ 
+  name: 'messageNotification', 
+  initialState, 
+  reducers: { 
+    setMessageNotification: ( state, action: PayloadAction<{ message: string; email: string }> ) => { 
+       state.message = action.payload.message; 
+       state.email = action.payload.email; 
+       localStorage.setItem(localStorageKey, JSON.stringify(state)); }, 
+    clearMessageNotification: (state) => { 
+       state.message = ''; 
+       state.email = ''; 
+       localStorage.removeItem(localStorageKey); 
+    }, 
+  }, 
+});
 
 export const { setMessageNotification, clearMessageNotification } = messageNotificationSlice.actions; export default messageNotificationSlice.reducer;
 
