6e26cd448144deb81fc6c24ca487248acfd6a1a1
Robert Marquez <legitem2023@gmail.com>
2025-04-04 07:43:49 +0800

    Update ReusableNotification.tsx

--- components/UI/ReusableNotification.tsx
+++ components/UI/ReusableNotification.tsx
@@ -9,7 +9,13 @@ const ReusableNotification: React.FC<PropsReusableNotification> = ({ number }) =
   return (
     <div
       className="ReusableNotification"
-      style={{ transform: `scale(${number > 0 ? 1 : 0})` }}
+      style={{
+       transform: `scale(${number > 0 ? 1 : 0})`,
+       opacity: number > 0 ? 1 : 0,
+       height: number > 0 ? "auto" : 0,
+       overflow: "hidden",
+       transition: "transform 0.3s, opacity 0.3s, height 0.3s",
+      }}
     >
       <div className="icon-container">
         <Icon icon="mdi:bell" width="20" height="20" className="icon-with-border" />
