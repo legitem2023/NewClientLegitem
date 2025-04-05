9a2c59795a47369706714f10d728c9edbd007ab3
Robert Marquez <legitem2023@gmail.com>
2025-04-05 14:51:14 +0800

    Update Price_strike.tsx

--- components/UI/Price_strike.tsx
+++ components/UI/Price_strike.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { formatter } from 'utils/scripts'
 
 type PropsPrice = {
-    item:any
+    item:any,
+    orig:any
 }
-const Price_strike:React.FC<PropsPrice> = ({item}) => {
+const Price_strike:React.FC<PropsPrice> = ({item,orig}) => {
   return (
   <div>  
-        <s className='thumbElements'>{formatter.format(item.price)}</s>
+      <s className='thumbElements'>{formatter.format(item.price)}</s>
+      <span className='thumbElements'>{formatter.format(orig)}</span>
   </div>
   )
 }
