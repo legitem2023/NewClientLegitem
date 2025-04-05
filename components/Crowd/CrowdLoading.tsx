783d2701df6f88994e592561e0b2cb3de5cdb948
Robert Marquez <legitem2023@gmail.com>
2025-04-05 18:10:54 +0800

    Update CrowdLoading.tsx

--- components/Crowd/CrowdLoading.tsx
+++ components/Crowd/CrowdLoading.tsx
@@ -1,86 +1,61 @@
-import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout'
-import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
-import React from 'react'
+import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout';
+import React from 'react';
+
+const LoadingBox = ({ height = '30px', width = '100%', borderRadius }: { height?: string, width?: string, borderRadius?: string }) => (
+  <div
+    className="loading-screen"
+    style={{
+      boxSizing: 'border-box',
+      height,
+      width,
+      borderRadius,
+    }}
+  />
+);
+
+const MessageSkeleton = ({ imgSize = '50px', marginBottom = '25px' }) => (
+  <div className="messagesLI" style={{ marginBottom }}>
+    <div>
+      <div className="messageSender">
+        <div className="messageSenderImgcont">
+          <LoadingBox height={imgSize} width={imgSize} borderRadius="100%" />
+        </div>
+        <LoadingBox />
+        <LoadingBox />
+      </div>
+      <div className="messageBody">
+        <LoadingBox height="200px" />
+      </div>
+      <div className="messageReactions">
+        <LoadingBox />
+        <LoadingBox />
+        <LoadingBox />
+      </div>
+    </div>
+  </div>
+);
+
 const CrowdLoading = () => {
   return (
-        <ReusableCenterLayout
-        child1={()=>(
-            <></>
-        )}
-        child2={()=>(
-            <div style={{display:"flex",flexDirection:"column"}} className="Messenger_inputs">
-                <div className='Messenger_inputs_fdiv'>
-                    <div className='loading-screen' style={{boxSizing:"border-box",height:"40px"}}></div>
-                </div>
-            </div>
-    )}
-        child3={()=>(
-            <div className='messagesUL' style={{boxShadow:"0.5px 0.5px 3px #c0c0c0"}}>
-                <div className='messagesLI' style={{marginBottom:"15px"}}>
-                    <div>
-                        <div className='messageSender'>
-                            <div className='messageSenderImgcont'>
-                                <div className='loading-screen' style={{width:"40px",height:"40px",borderRadius:"100%"}}></div>
-                            </div>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"30px",width:"100%"}}></div>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"30px",width:"100%"}}></div>
-                        </div>
-                        <div className='messageBody'>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"200px"}}></div>
-                        </div>
-                        <div className='messageReactions'>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-
-                        </div>
-                    </div>
-                </div>
-                <div className='messagesLI' style={{marginBottom:"25px"}}>
-                    <div>
-                        <div className='messageSender'>
-                            <div className='messageSenderImgcont'>
-                            <div className='loading-screen' style={{width:"50px",height:"50px",borderRadius:"100%"}}></div>
-                            </div>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"30px",width:"100%"}}></div>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"30px",width:"100%"}}></div>
-                        </div>
-                        <div className='messageBody'>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"200px"}}></div>
-                        </div>
-                        <div className='messageReactions'>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-
-                        </div>
-                    </div>
-                </div>
-                <div className='messagesLI' style={{marginBottom:"25px"}}>
-                    <div>
-                        <div className='messageSender'>
-                            <div className='messageSenderImgcont'>
-                            <div className='loading-screen' style={{width:"50px",height:"50px",borderRadius:"100%"}}></div>
-                            </div>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"30px",width:"100%"}}></div>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"30px",width:"100%"}}></div>
-                        </div>
-                        <div className='messageBody'>
-                            <div className='loading-screen' style={{boxSizing:"border-box",height:"200px"}}></div>
-                        </div>
-                        <div className='messageReactions'>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-                        <div className='loading-screen' style={{boxSizing:"border-box",height:"30px"}}></div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )}
-        child4={()=>(<></>)}
+    <ReusableCenterLayout
+      child1={() => <></>}
+      child2={() => (
+        <div style={{ display: 'flex', flexDirection: 'column' }} className="Messenger_inputs">
+          <div className="Messenger_inputs_fdiv">
+            <LoadingBox height="40px" />
+          </div>
+        </div>
+      )}
+      child3={() => (
+        <div className="messagesUL" style={{ boxShadow: '0.5px 0.5px 3px #c0c0c0' }}>
+          <MessageSkeleton imgSize="40px" marginBottom="15px" />
+          <MessageSkeleton />
+          <MessageSkeleton />
+        </div>
+      )}
+      child4={() => <></>}
     />
-  )
-}
+  );
+};
 
-export default CrowdLoading
+export default CrowdLoading;
