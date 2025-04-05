ef3c636e3a9c3ed26d5bb6345a7ab508e8280a61
Robert Marquez <legitem2023@gmail.com>
2025-04-05 14:42:15 +0800

    Update ReviewLoading.tsx

--- components/Reviews/ReviewLoading.tsx
+++ components/Reviews/ReviewLoading.tsx
@@ -1,1 +1,61 @@
-nnn
+import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout'
+import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
+import React from 'react'
+
+const ReviewLoading = () => {
+  return (
+            <ReusableCenterLayout
+                child1={()=>(
+                <div style={{padding:'2px'}}>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                </div>    
+                )}
+                child2={()=>(
+                <div style={{padding:'2px'}}>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                  <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #000000'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  </div>
+                </div>
+                )}
+                child3={()=>(
+                <div style={{padding:'2px'}}>
+                </div>
+                )}
+                child4={()=>(<></>)}
+            />
+  )
+}
+
+export default ReviewLoading
