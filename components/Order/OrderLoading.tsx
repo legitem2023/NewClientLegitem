0c603c12d765267a58fa98540e526ae026f4e037
Robert Marquez <legitem2023@gmail.com>
2025-04-05 15:30:22 +0800

    Update OrderLoading.tsx

--- components/Order/OrderLoading.tsx
+++ components/Order/OrderLoading.tsx
@@ -3,6 +3,10 @@ import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
 import React from 'react'
 
 const OrderLoading = () => {
+  let clipPathA = "polygon(0% 0%, 88% 0%, 100% 50%, 88% 100%, 0% 100%, 12% 50%)"; // Middle tabs (arrow on both sides)      
+  let clipPathB = "polygon(0% 0%, 88% 0%, 100% 50%, 88% 100%, 0% 100%)"; // First tab (flat left, arrow right)        
+  let clipPathC = "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 12% 50%)"; // Last tab (arrow left, flat right)
+          
   return (
             <ReusableCenterLayout
                 child1={()=>(
@@ -14,14 +18,11 @@ const OrderLoading = () => {
                              marginTop:'5px',
                              marginBottom:'5px',
                              boxShadow:'0.5px 0.5px 3px #000000'}}>
-                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                </div>
+                  <div className='loading-screen' style={{boxSizing:"border-box",clipPathA}}></div>
+                  <div className='loading-screen' style={{boxSizing:"border-box",clipPathB}}></div>
+                  <div className='loading-screen' style={{boxSizing:"border-box",clipPathB}}></div>                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  <div className='loading-screen' style={{boxSizing:"border-box",clipPathB}}></div>                  <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                  <div className='loading-screen' style={{boxSizing:"border-box",clipPathC}}></div>                </div>
                 </div>
                 )}
                 child2={()=>(
