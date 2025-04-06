29634bc5838ba1f90c68fd7e5636223a208174be
Robert Marquez <legitem2023@gmail.com>
2025-04-06 08:34:24 +0800

    Update RelatedProducts.tsx

--- components/Products/ProductView/RelatedProducts.tsx
+++ components/Products/ProductView/RelatedProducts.tsx
@@ -6,6 +6,7 @@ import Name from 'components/UI/Name';
 import Price from 'components/UI/Price';
 import Price_strike from 'components/UI/Price_strike';
 import ReusableLabel from 'components/UI/ReusableLabel';
+import ReusableCard from 'components/UI/ReusableCard';
 import Sold from 'components/UI/Sold';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -48,14 +49,26 @@ const RelatedProducts = ({ data }) => {
     console.log([item]);
     dispatch(setViewedProd([item]));
   };
+
+  interface ReusableCardProps {
+  item: any;
+  view: (item: any) => void;
+  imageSource: (item: any) => string;
+  handleError: (e: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+  handleLoading: (e: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+}
   return (
     <div className='MainView_Rchild'>
       <ReusableLabel icn='' label='Related Product'/>
       <div className='MainView_RelatedProducts'>
         <div>
           {data.slice(0, take).map((item: any, idx) => (
-            <div key={idx} className='MainView_RelatedProductsThumbs' onClick={() => view(item)}>
-              {/* <Link href={`${path}Products/${item.id}`}> */}
+      <ReusableCard item={item} 
+                    view={view(item)}
+                    imageSource={imageSource(item)}
+                    handleError={handleError}
+                    handleLoading={handleLoading} />
+      {/* <div key={idx} className='MainView_RelatedProductsThumbs' onClick={() => view(item)}>
               <Image
                 src={imageSource(item)}
                 height='200'
@@ -66,22 +79,21 @@ const RelatedProducts = ({ data }) => {
                 onError={handleError} // Use memoized error handler
                 onClick={handleLoading} // Use memoized loading handler
               />
-              {/* </Link> */}
               <div className='thumbnailTextContainer'>
                 <Element Label="Name" value={item.name} />
                 <Element Label="Color" value={item.color} />
                 <Element Label="Size" value={item.size} />
                 <Element Label="Sold" value={item.TotalSoldItems ? item.TotalSoldItems : '0'} />
                 {item.discount > 0 ? <Price_strike item={item} /> : <Price item={item} />}
-                {/*item.discount > 0 ? <Discounted item={item} /> : ""*/}
                 <div className='Rates'>
                   <div className='ViewsLikes'>
                     <Ratings data={item.TotalRatings > 0 ? item.TotalRatings : 0} count={item} />
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            </div>*/}
+          ))
+          }
         </div>
         <div className='LoadmoreContainer'>
           {data.length <= take ? (
