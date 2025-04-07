7e1a0fa696f4e5d4394ae0a98121178cb4d6cb13
Robert Marquez <legitem2023@gmail.com>
2025-04-07 22:16:15 +0800

    Update LikesLoading.tsx

--- components/Likes/LikesLoading.tsx
+++ components/Likes/LikesLoading.tsx
@@ -7,11 +7,16 @@ const LikesLoading = () => {
       child1={() =><></>}
       child2={() =><></>}
       child3={() =>(
-        <div className="LikeContainer">
-          {[...Array(10)].map((_, index) => (
-            <div key={index} className='loading-screen' style={{height:150,margin:"4px"}}></div>
-          ))}
-        </div>)
+        <div
+          style={{ overflowY: 'auto', height: 'auto', scrollbarWidth: 'none' }} // Set height to auto
+        >
+         <div className="LikeContainer">
+           {[...Array(10)].map((_, index) => (
+             <div key={index} className='loading-screen' style={{height:150,margin:"4px"}}></div>
+           ))}
+         </div>
+        </div>
+          )
       }
       child4={() =><></>}
     />
