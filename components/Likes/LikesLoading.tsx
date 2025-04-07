b4a0e6056ac5b7d87cdec202b5a09630c5200c45
Robert Marquez <legitem2023@gmail.com>
2025-04-07 17:05:51 +0800

    Update LikesLoading.tsx

--- components/Likes/LikesLoading.tsx
+++ components/Likes/LikesLoading.tsx
@@ -4,24 +4,24 @@ import React from 'react'
 const LikesLoading = () => {
   return (
     <ReusableCenterLayout
-      child1={
+      child1={() =>
         <div style={{height:"45px", gap:"2px", marginTop:'5px', marginBottom:'5px', boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
           <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
         </div>
       }
-      child2={
+      child2={() =>
         <div className="card" style={{height:"200px", gap:"2px", marginTop:'5px', marginBottom:'5px', boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
           <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
         </div>
       }
-      child3={
+      child3={() =>
         <div className="LikeContainer">
           {[...Array(10)].map((_, index) => (
             <div key={index} className='loading-screen' style={{height:150}}></div>
           ))}
         </div>
       }
-      child4={<></>}
+      child4={() =><></>}
     />
   )
 }
