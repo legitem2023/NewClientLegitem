aff8bba8e2aba677c577fc9b03b2ff6e0d63e805
Robert Marquez <legitem2023@gmail.com>
2025-04-07 14:08:35 +0800

    Update LikesData.tsx

--- components/Likes/LikesData.tsx
+++ components/Likes/LikesData.tsx
@@ -1,21 +1,19 @@
+import React, { useEffect, useCallback, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { useSelector, useDispatch } from 'react-redux';
+
 import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout';
 import Loading from 'components/Partial/LoadingAnimation/Loading';
 import ReusableServerDown from 'components/UI/ReusableServerDown';
-import UniversalContainerItem_Likes from 'components/UI/UniversalContainerItem_Likes';
 import ReusableCard from 'components/UI/ReusableCard';
-import { setViewedProd } from 'Redux/viewedProdSlice';
 import { READ_LIKES } from 'graphql/queries';
-import { useQuery } from '@apollo/client';
-import React,{ useEffect,useCallback } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
+import { setViewedProd } from 'Redux/viewedProdSlice';
 import { imageSource } from 'utils/scripts';
 
 const LikesData = () => {
-  const path = process.env.NEXT_PUBLIC_PATH
-
-  const cookie = useSelector((state:any)=> state.cookie.cookie);
-  const [useLoading, setLoading] = useState(false);
   const dispatch = useDispatch();
+  const cookie = useSelector((state: any) => state.cookie.cookie);
+  const [useLoading, setLoading] = useState(false);
 
   const fallbackImage = `https://hokei-storage.s3.ap-northeast-1.amazonaws.com/images/Legit/IconImages/Legitem-svg.svg`;
 
@@ -28,47 +26,47 @@ const LikesData = () => {
   );
 
   const handleLoading = useCallback(() => {
-    // Add custom loading logic if needed
+    // Optional: add logic here
   }, []);
 
-  const LoadMoreData = () => {
-    setTake((prev) => prev + 5);
-    setLoading(true);
-  };
-
   const view = (item: any) => {
-    dispatch(setViewedProd([item]));
+   // dispatch(setViewedProd([item]));
   };
-  
-  const {data:LikeData,loading:LikeLoading,error:LikeError} = useQuery(READ_LIKES,{
-    variables:{
-      accountEmail:cookie.emailAddress
-    }
-  });
-  if(LikeLoading) return <Loading/> 
-  if(LikeError) return <ReusableServerDown/>
-  return (
-    <ReusableCenterLayout child1={()=><></>} 
-    child2={()=>(
-      <div className='NewsContainer'>
-      {LikeData.readLikes.length > 0?LikeData.readLikes?.map((item: any, idx: number) => (
-        {/* <UniversalContainerItem_Likes key={idx} 
-                                      item={item} 
-                                      title={item.productCode} 
-                                      thumbnail={imageSource(item.thumbnail)}  
-                                      index={idx}/>*/}
-        <ReusableCard
-              key={idx}
-              item={item}
-              view={() => {view(item)}}
-              imageSource={() => imageSource(item)}
-              handleError={() => handleError}
-              handleLoading={() => handleLoading}
-            />
-      )):(<h1>No Data</h1>)}
-    </div>
-    )} child3={()=><></>} child4={()=><></>}/>
-  )
-}
 
-export default LikesData
+  const { data: LikeData, loading: LikeLoading, error: LikeError } = useQuery(READ_LIKES, {
+    variables: {
+      accountEmail: cookie.emailAddress,
+    },
+  });
+
+  if (LikeLoading) return <Loading />;
+  if (LikeError) return <ReusableServerDown />;
+
+  return (
+    <ReusableCenterLayout
+      child1={() => <></>}
+      child2={() => (
+        <div className="NewsContainer">
+          {LikeData?.readLikes?.length > 0 ? (
+            LikeData.readLikes.map((item: any, idx: number) => (
+              <ReusableCard
+                key={idx}
+                item={item}
+                view={() => view(item)}
+                imageSource={() => imageSource(item)}
+                handleError={handleError}
+                handleLoading={handleLoading}
+              />
+            ))
+          ) : (
+            <h1>No Data</h1>
+          )}
+        </div>
+      )}
+      child3={() => <></>}
+      child4={() => <></>}
+    />
+  );
+};
+
+export default LikesData;
