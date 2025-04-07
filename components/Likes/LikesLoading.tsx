81b1a7fc048e740ff1052147490a884621b8d42f
Robert Marquez <legitem2023@gmail.com>
2025-04-07 16:43:05 +0800

    Create LikesLoading.tsx

---
+++ components/Likes/LikesLoading.tsx
@@ -0,0 +1,38 @@
+import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout'
+import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
+import React from 'react'
+
+const LikesLoading = () => {
+  return (
+            <ReusableCenterLayout
+                child1={()=>(
+                <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                </div>
+                )}
+                child2={()=>(
+                <div className="card" style={{height:"200px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
+                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+                </div>
+                )}
+                child3={()=>(
+                    <div className="LikeContainer"> 
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      <div className='loading-screen' style={{height:150px}}></div>
+                      
+                    </div>
+                )}
+                child4={()=>(<></>)}
+            />
+  )
+}
+
+export default LikesLoading
