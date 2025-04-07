a7f0d95b03bdadf329868a1d0ddd329ee303d0ea
Robert Marquez <legitem2023@gmail.com>
2025-04-07 18:07:15 +0800

    Update ReusableCard.tsx

--- components/UI/ReusableCard.tsx
+++ components/UI/ReusableCard.tsx
@@ -37,6 +37,7 @@ const ReusableCard: React.FC<ReusableCardProps> = ({ item, view, imageSource, ha
       />      
       </div>
       <div className='thumbnailTextContainer' style={{ display:'flex',
+                                                       flexDirection:'column',
                                                        justifyContent:'flex-start',
                                                        backgroundColor:'#f1f1f1'}}>
         <Element Label="Name" value={item.name} />
