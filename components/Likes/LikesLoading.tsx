769b4a137af162b4602fa2c834c70df88ac33c2b
Robert Marquez <legitem2023@gmail.com>
2025-04-07 16:55:45 +0800

    Update LikesLoading.tsx

--- components/Likes/LikesLoading.tsx
+++ components/Likes/LikesLoading.tsx
@@ -1,37 +1,28 @@
 import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout'
-import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
 import React from 'react'
 
 const LikesLoading = () => {
   return (
-            <ReusableCenterLayout
-                child1={()=>(
-                <div style={{height:"45px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
-                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                </div>
-                )}
-                child2={()=>(
-                <div className="card" style={{height:"200px",gap:"2px",marginTop:'5px',marginBottom:'5px',boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
-                    <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-                </div>
-                )}
-                child3={()=>(
-                    <div className="LikeContainer"> 
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      <div className='loading-screen' style={{height:150px}}></div>
-                      
-                    </div>
-                )}
-                child4={()=>(<></>)}
-            />
+    <ReusableCenterLayout
+      child1={
+        <div style={{height:"45px", gap:"2px", marginTop:'5px', marginBottom:'5px', boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
+          <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+        </div>
+      }
+      child2={
+        <div className="card" style={{height:"200px", gap:"2px", marginTop:'5px', marginBottom:'5px', boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
+          <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
+        </div>
+      }
+      child3={
+        <div className="LikeContainer">
+          {[...Array(10)].map((_, index) => (
+            <div key={index} className='loading-screen' style={{height:150}}></div>
+          ))}
+        </div>
+      }
+      child4={<></>}
+    />
   )
 }
 
