68b0c6ddbf3ba6e95ebe0a0ac4afe9ac1fbed5a2
Robert Marquez <legitem2023@gmail.com>
2025-04-07 17:15:30 +0800

    Update LikesLoading.tsx

--- components/Likes/LikesLoading.tsx
+++ components/Likes/LikesLoading.tsx
@@ -5,19 +5,15 @@ const LikesLoading = () => {
   return (
     <ReusableCenterLayout
       child1={() =>(
-        <div style={{height:"45px", gap:"2px", marginTop:'5px', marginBottom:'5px', boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
-          <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-        </div>)
+        <><>)
       }
       child2={() =>(
-        <div className="card" style={{height:"200px", gap:"2px", marginTop:'5px', marginBottom:'5px', boxShadow:'0.5px 0.5px 3px #c0c0c0'}}>
-          <div className='loading-screen' style={{boxSizing:"border-box"}}></div>
-        </div>)
+        <><>)
       }
       child3={() =>(
         <div className="LikeContainer">
           {[...Array(10)].map((_, index) => (
-            <div key={index} className='loading-screen' style={{height:150}}></div>
+            <div key={index} className='loading-screen' style={{height:150,margin:"4px"}}></div>
           ))}
         </div>)
       }
