c5309a57928b1331b900a32a543143c68bd195bc
Robert Marquez <legitem2023@gmail.com>
2025-04-13 16:14:45 +0800

    Update AddCartCmd.tsx

--- components/Commands/AddCartCmd.tsx
+++ components/Commands/AddCartCmd.tsx
@@ -1,42 +1,41 @@
 import { Icon } from '@iconify/react'
-import { ShoppingCartContext } from 'components/context/ShoppingCartProvider';
-import React, { useContext } from 'react'
-import { useDispatch } from 'react-redux';
-import { addToCart } from 'Redux/cartSlice';
-import DataManager from 'utils/DataManager';
-import { Cart } from 'utils/scripts';
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addToCart } from 'Redux/cartSlice'
+import DataManager from 'utils/DataManager'
 
 type PropsAddCartCmd = {
-    item: any
+  item: any
 }
-const AddCartCmd:React.FC<PropsAddCartCmd> = (item) => {
-    
-    const Manager = new DataManager();
-    const dispatch = useDispatch(); 
-    const AddToCart = (item) =>{
-        console.log(item.item);
-        const cartData:any = {
-            id: item.item.id, // You can change this to number if IDs are numeric
-            productCode:item.item.productCode,
-            image:item.item.image,
-            name: item.item.name,
-            color:item.item.color,
-            size:item.item.size,
-            price: parseInt(item.item.price),
-            quantity: 1
-          }
-            console.log(cartData);
-            dispatch(addToCart([cartData]));
-            Manager.Success("Added to cart");
-        })
-     } 
-       
+
+const AddCartCmd: React.FC<PropsAddCartCmd> = ({ item }) => {
+  const Manager = new DataManager()
+  const dispatch = useDispatch()
+
+  const AddToCart = () => {
+    console.log(item)
+    const cartData: any = {
+      id: item.id,
+      productCode: item.productCode,
+      image: item.image,
+      name: item.name,
+      color: item.color,
+      size: item.size,
+      price: parseInt(item.price),
+      quantity: 1,
+    }
+
+    console.log(cartData)
+    dispatch(addToCart([cartData]))
+    Manager.Success('Added to cart')
+  }
+
   return (
-    <Icon 
-    icon='mdi:cart' 
-    className='iconify_cart' 
-    onClick={() => AddToCart(item)}
-  />
+    <Icon
+      icon='mdi:cart'
+      className='iconify_cart'
+      onClick={AddToCart}
+    />
   )
 }
 
