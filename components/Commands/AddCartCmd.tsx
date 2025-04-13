9d98b2f282e6128d2ed525a377e11523683e8550
Robert Marquez <legitem2023@gmail.com>
2025-04-13 15:06:21 +0800

    Update AddCartCmd.tsx

--- components/Commands/AddCartCmd.tsx
+++ components/Commands/AddCartCmd.tsx
@@ -14,6 +14,7 @@ const AddCartCmd:React.FC<PropsAddCartCmd> = (item) => {
     const Manager = new DataManager();
     const dispatch = useDispatch(); 
     const AddToCart = (item) =>{
+        console.log([item]);
         const cartData:any = [item].map((item:any)=>{
           return {
             id: item.id, // You can change this to number if IDs are numeric
