6b8d8d14afc69fe7724a73fe4cc666f0469eab79
Robert Marquez <legitem2023@gmail.com>
2025-04-16 15:14:18 +0800

    Update RelatedSize.tsx

--- components/Products/ProductView/RelatedSize.tsx
+++ components/Products/ProductView/RelatedSize.tsx
@@ -8,10 +8,11 @@ import Link from 'next/link'
 import { useDispatch } from 'react-redux'
 import { setViewedProd } from 'Redux/viewedProdSlice'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
-import 'swiper/css/pagination';
-import 'swiper/css';
-import 'swiper/css/scrollbar';
+import { Navigation, Pagination, Thumbs, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/thumbs";
 import ReusableFirstLetterImage from 'components/Reusable/ReusableFirstLetterImage';
 const RelatedSize = ({styleCode,currentsize}) => {
     const {data,loading,error} = useQuery(GET_CHILD_INVENTORY_RELATED_COLOR_SIZE,{
@@ -27,14 +28,26 @@ const RelatedSize = ({styleCode,currentsize}) => {
         dispatch(setViewedProd([item]))
       }
     return (
-     <div className="colorSelection" style={{ display:'flex',flexDirection:'column' }}>
+        {/*     <div className="colorSelection" style={{ display:'flex',flexDirection:'column' }}> */}
+        <Swiper
+        modules={[Navigation,Thumbs, Autoplay]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={loop}
+        modules={[Navigation, Thumbs]}
+        slidesPerView={4}
+        spaceBetween={10}
+        watchSlidesProgress
+        autoplay={autoplay ? { delay: 3000, disableOnInteraction: false } : false}
+        thumbs={{ swiper: thumbsSwiper }}
+      >
   {data.getChildInventory_details.map((item: any) => (
-      <div key={item.id} style={{ display: "flex", flexDirection: "column" }} onClick={() => view(item)}>
+      <SwiperSlide key={item.id} style={{ display: "flex", flexDirection: "column" }} onClick={() => view(item)}>
         <button style={{height:'35px',width:'50%',border:currentsize===item.size?'solid 2px brown':'solid 2px white'}}>{item.size}</button>
-      </div>
+      </SwiperSlide>
     )
   )}
-</div>
+</Swiper>
   )
 }
 export default RelatedSize
