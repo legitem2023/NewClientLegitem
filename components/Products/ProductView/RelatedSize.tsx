348c4ab4b1d14df6c1753d4661b6f54b2d687f3d
Robert Marquez <legitem2023@gmail.com>
2025-04-16 15:30:58 +0800

    Update RelatedSize.tsx

--- components/Products/ProductView/RelatedSize.tsx
+++ components/Products/ProductView/RelatedSize.tsx
@@ -1,48 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GET_CHILD_INVENTORY_RELATED_COLOR_SIZE } from 'graphql/queries'
 import { useQuery } from '@apollo/client'
-import { i } from 'vitest/dist/reporters-yx5ZTtEV'
 import { imageSourceGallery } from 'utils/scripts'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useDispatch } from 'react-redux'
 import { setViewedProd } from 'Redux/viewedProdSlice'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Thumbs, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/thumbs";
-import ReusableFirstLetterImage from 'components/Reusable/ReusableFirstLetterImage';
-const RelatedSize = ({styleCode,currentsize}) => {
-    const {data,loading,error} = useQuery(GET_CHILD_INVENTORY_RELATED_COLOR_SIZE,{
-        variables:{
-            styleCode:styleCode
-        }
-    })
-    const dispatch = useDispatch();
-    const path = process.env.NEXT_PUBLIC_PATH;
-    if(loading) return;
-    if(error) return
-      const view = (item:any) =>{
-        dispatch(setViewedProd([item]))
-      }
-    return (
-        {/*     <div className="colorSelection" style={{ display:'flex',flexDirection:'column' }}> */}
-        <Swiper
-        modules={[Navigation, Thumbs]}
-        slidesPerView={4}
-        spaceBetween={10}
-        watchSlidesProgress
-        autoplay={autoplay ? { delay: 3000, disableOnInteraction: false } : false}
-        thumbs={{ swiper: thumbsSwiper }}>
-  {data.getChildInventory_details.map((item: any) => (
-      <SwiperSlide key={item.id} style={{ display: "flex", flexDirection: "column" }} onClick={() => view(item)}>
-        <button style={{height:'35px',width:'50%',border:currentsize===item.size?'solid 2px brown':'solid 2px white'}}>{item.size}</button>
-      </SwiperSlide>
-    )
-  )}
-</Swiper>
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Thumbs, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/thumbs'
+import ReusableFirstLetterImage from 'components/Reusable/ReusableFirstLetterImage'
+
+interface RelatedSizeProps {
+  styleCode: string
+  currentsize: string
+}
+
+const RelatedSize: React.FC<RelatedSizeProps> = ({ styleCode, currentsize }) => {
+  const { data, loading, error } = useQuery(GET_CHILD_INVENTORY_RELATED_COLOR_SIZE, {
+    variables: {
+      styleCode: styleCode,
+    },
+  })
+
+  const dispatch = useDispatch()
+  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+
+  const path = process.env.NEXT_PUBLIC_PATH
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error loading related sizes.</p>
+
+  const view = (item: any) => {
+    dispatch(setViewedProd([item]))
+  }
+
+  return (
+    <Swiper
+      modules={[Navigation, Thumbs, Autoplay]}
+      slidesPerView={4}
+      spaceBetween={10}
+      watchSlidesProgress
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      thumbs={{ swiper: thumbsSwiper }}
+    >
+      {data?.getChildInventory_details?.map((item: any) => (
+        <SwiperSlide
+          key={item.id}
+          style={{ display: 'flex', flexDirection: 'column' }}
+          onClick={() => view(item)}
+        >
+          <button
+            style={{
+              height: '35px',
+              width: '50%',
+              border: currentsize === item.size ? 'solid 2px brown' : 'solid 2px white',
+            }}
+          >
+            {item.size}
+          </button>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   )
 }
+
 export default RelatedSize
