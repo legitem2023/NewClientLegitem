daa1ef4aa96de5d9dcd9f8211291f29b5acb4563
Robert Marquez <legitem2023@gmail.com>
2025-04-21 15:54:42 +0800

    Update ReusableCustomCarousel.tsx

--- components/Reusable/ReusableCustomCarousel.tsx
+++ components/Reusable/ReusableCustomCarousel.tsx
@@ -4,17 +4,7 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
-type CarouselItem = {
-  Name: string;
-  image: string;
-};
-
-type Props = {
-  data: CarouselItem[];
-  onImageClick?: (item: CarouselItem) => void;
-};
-
-const ReusableCustomCarousel: React.FC<Props> = ({ data, onImageClick = () => {} }) => {
+const ReusableCustomCarousel = ({ data }) => {
   const galleryItems = data.map((item) => ({
     original: item.image,
     thumbnail: item.image,
@@ -27,13 +17,6 @@ const ReusableCustomCarousel: React.FC<Props> = ({ data, onImageClick = () => {}
         items={galleryItems}
         showPlayButton={true}
         showFullscreenButton={true}
-        onClick={(e) => {
-          const index = (e?.target as any)?.closest('[data-index]')?.getAttribute('data-index');
-          if (index !== null && index !== undefined) {
-            const idx = parseInt(index);
-            if (!isNaN(idx)) onImageClick(data[idx]);
-          }
-        }}
       />
     </div>
   );
