c1f50c8c3a281b69d981e28ccecfc5df0fdf0cd1
Robert Marquez <legitem2023@gmail.com>
2025-04-21 16:09:28 +0800

    Update ReusableCustomCarousel.tsx

--- components/Reusable/ReusableCustomCarousel.tsx
+++ components/Reusable/ReusableCustomCarousel.tsx
@@ -5,6 +5,33 @@ import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
 const ReusableCustomCarousel = ({ data }) => {
+
+const fallbackData = [
+  {
+    "Name": "Product 1",
+    "image": "https://example.com/images/product1.jpg"
+  },
+  {
+    "Name": "Product 2",
+    "image": "https://example.com/images/product2.jpg"
+  },
+  {
+    "Name": "Product 3",
+    "image": "https://example.com/images/product3.jpg"
+  },
+  {
+    "Name": "Product 4",
+    "image": "https://example.com/images/product4.jpg"
+  },
+  {
+    "Name": "Product 5",
+    "image": "https://example.com/images/product5.jpg"
+  },
+  {
+    "Name": "Product 6",
+    "image": "https://example.com/images/product6.jpg"
+  }];
+  
   const galleryItems = data.map((item) => ({
     original: item.image,
     thumbnail: item.image,
