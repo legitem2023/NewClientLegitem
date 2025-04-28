c4e52c70b86d542fee3ae32a48b820f78b3d0c8a
Robert Marquez <legitem2023@gmail.com>
2025-04-28 08:43:33 +0800

    Update ProductTabs.tsx

--- components/Products/ProductView/ProductTabs.tsx
+++ components/Products/ProductView/ProductTabs.tsx
@@ -95,7 +95,7 @@ const ProductTabs:React.FC<PropsProductTabs> = ({data}:any) =>{
   
   const optional_rendering_tab_gallery = () =>{
     if (isActive === "Gallery") {
-      return (<ReusableCustomCarousel data={galleryItems}/>);
+      return (<ReusableCustomCarousel data={galleryItems} showthumbs={false} />);
     }
   }
   const optional_rendering_tab_vto = () =>{
