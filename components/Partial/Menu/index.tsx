038057b8297b3928d6038e47d377aaca44442af0
Robert Marquez <legitem2023@gmail.com>
2025-05-10 10:59:17 +0800

    Update index.tsx

--- components/Partial/Menu/index.tsx
+++ components/Partial/Menu/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React,{ useRef } from 'react';
 import menuList from '../../../json/menu.json';
 import { Icon } from '@iconify/react';
 import { useQuery } from '@apollo/client';
