ab607ba6ae459d9906fc9389d68a6fa0b4d39aca
Robert Marquez <legitem2023@gmail.com>
2025-05-12 10:42:09 +0800

    Update index.tsx

--- components/Partial/Menu/index.tsx
+++ components/Partial/Menu/index.tsx
@@ -38,7 +38,7 @@ const Menu: React.FC = () => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        dispatch(setDrawer(false));
+        dispatch(setDrawer(true));
       }
     };
     document.addEventListener("mousedown", handleClickOutside);
