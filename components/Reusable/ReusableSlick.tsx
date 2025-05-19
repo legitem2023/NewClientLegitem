684f9c8df087e39bbdeb2ae5dd24b33bc853456c
Robert Marquez <legitem2023@gmail.com>
2025-05-19 10:54:33 +0800

    Update ReusableSlick.tsx

--- components/Reusable/ReusableSlick.tsx
+++ components/Reusable/ReusableSlick.tsx
@@ -5,24 +5,31 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css/pagination';
 import 'swiper/css';
 import Element_Title from '../UI/Element_Title';
-// import 'swiper/css/scrollbar';
 import ModelViewer from "../Partial/ThreeJS/ModelViewer";
-const ReusableSlick = ({data}) => {    
-    return (
-      <div className="card" style={{height:"auto",position:"relative",padding:"10px"}}>
-            <Swiper
-            slidesPerView={3}
-            spaceBetween={2}
-            style={{position:"relative",width:"100%"}}
-            loop={false}>
-                  {data.map((item:any,idx:number)=>(
-                <SwiperSlide key={idx} style={backgroundColor:"transparent"}>
-                    <img style={{borderRadius:'10px',margin:'10px'}} height={200} width={200} src={item.image} />
-                    <Element_Title Label={''} value={item.name}/>
-                </SwiperSlide>   
-                  ))}
-          </Swiper>
-      </div>
+
+const ReusableSlick = ({ data }) => {
+  return (
+    <div className="card" style={{ height: "auto", position: "relative", padding: "10px" }}>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={2}
+        style={{ position: "relative", width: "100%" }}
+        loop={false}
+      >
+        {data.map((item: any, idx: number) => (
+          <SwiperSlide key={idx} style={{ backgroundColor: "transparent" }}>
+            <img
+              style={{ borderRadius: '10px', margin: '10px' }}
+              height={200}
+              width={200}
+              src={item.image}
+              alt={item.name}
+            />
+            <Element_Title Label={''} value={item.name} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   )
 }
 
