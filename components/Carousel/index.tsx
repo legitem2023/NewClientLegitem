fc5d1c854f8d69abab16a22f9e6cc3614442a00f
Robert Marquez <legitem2023@gmail.com>
2025-05-19 22:25:16 +0800

    Update index.tsx

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -5,85 +5,120 @@ import { GET_CATEGORY } from "graphql/queries";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import React from "react";
-import {StackedCarousel,ResponsiveContainer} from "react-stacked-center-carousel";
+import { StackedCarousel, ResponsiveContainer } from "react-stacked-center-carousel";
 import { imageSource, imageSource_category } from "utils/scripts";
 
-export default function Carousel({data,fromData}) {
-  const ref:any = React.useRef(StackedCarousel);
+export default function Carousel({ data, fromData }) {
+  const ref: any = React.useRef(StackedCarousel);
   const imgPath = process.env.NEXT_PUBLIC_SERVER_PRODUCT_IMAGE_PATH || '';
   const pathname = usePathname();
   const Products = pathname.startsWith('/Products');
 
-  const Card = (props:any) => {
-    const { data, dataIndex }:any = props;
+  const Card = (props: any) => {
+    const { data, dataIndex }: any = props;
     const { image } = data[dataIndex];
 
     return (
-      <div>
       <div
         style={{
           width: "100%",
-          height:300,
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           userSelect: "none",
-          backgroundColor:"#c0c0c0"
+          backgroundColor: "#c0c0c0",
+          position: "relative"
         }}
         className="my-slide-component"
       >
         <Image
-          height="100"
-          width="200"
+          height={200}
+          width={400}
           alt={dataIndex}
           className="carouselImage"
           draggable={false}
           priority={true}
           src={image}
         />
-        <span style={{ color: "#000000",
-                    position: "absolute", 
-                    top: "0px",
-                    right:"0px",
-                    margin:"10px",
-                    padding:"5px",
-                    boxShadow:"0.5px 0.5px 3px #000000",
-                    backgroundColor:"rgb(249 220 206)" }}>{data[dataIndex].Name}</span>
-      </div>
+        <span
+          style={{
+            color: "#000000",
+            position: "absolute",
+            top: "0px",
+            right: "0px",
+            margin: "10px",
+            padding: "5px",
+            boxShadow: "0.5px 0.5px 3px #000000",
+            backgroundColor: "rgb(249 220 206)"
+          }}
+        >
+          {data[dataIndex].Name}
+        </span>
       </div>
     );
   };
 
-
-// if(Products) return
   return (
-  <div className="card" style={{aspectRatio:'5 / 3', width:'100%',display:'flex',alignItems:'center',justifyContent:'center'}}>
-    <div style={{ width: "100%", position: "relative" }}>
-      <ResponsiveContainer
-        carouselRef={ref}
-        render={(parentWidth, carouselRef) => {
-          let currentVisibleSlide = 5;
-          return (
-            <StackedCarousel
-              ref={carouselRef}
-              slideComponent={Card}
-              slideWidth={parentWidth < 200 ? parentWidth : 400}
-              carouselWidth={parentWidth}
-              data={data}
-              customScales = {[1, 0.7, 0.5, 0.2, 0.1]}
-              currentVisibleSlide={currentVisibleSlide}
-              maxVisibleSlide={7}
-              useGrabCursor
-            />
-          );
-        }}
-      />
-        <div className="card-button left" onClick={() => ref.current?.goBack()}>
-              <Icon icon='ic:sharp-double-arrow' style={{ fontSize: 30, color: "rgb(87,39,0)" }} />
+    <div
+      className="card"
+      style={{
+        width: '100%',
+        aspectRatio:'5 / 3',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div style={{ width: "100%", position: "relative" }}>
+        <ResponsiveContainer
+          carouselRef={ref}
+          render={(parentWidth, carouselRef) => {
+            let currentVisibleSlide = 5;
+            return (
+              <StackedCarousel
+                ref={carouselRef}
+                slideComponent={Card}
+                slideWidth={parentWidth < 200 ? parentWidth : 400}
+                carouselWidth={parentWidth}
+                data={data}
+                customScales={[1, 0.7, 0.5, 0.2, 0.1]}
+                currentVisibleSlide={currentVisibleSlide}
+                maxVisibleSlide={7}
+                useGrabCursor
+              />
+            );
+          }}
+        />
+        <div
+          className="card-button left"
+          onClick={() => ref.current?.goBack()}
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '10px',
+            transform: 'translateY(-50%)',
+            cursor: 'pointer',
+            zIndex: 1
+          }}
+        >
+          <Icon icon='ic:sharp-double-arrow' style={{ fontSize: 30, color: "rgb(87,39,0)", transform: "rotate(180deg)" }} />
         </div>
-        <div className="card-button right" onClick={() => ref.current?.goNext()}>
-              <Icon icon='ic:sharp-double-arrow' style={{ fontSize: 30, color: "rgb(87,39,0)" }} />
+        <div
+          className="card-button right"
+          onClick={() => ref.current?.goNext()}
+          style={{
+            position: 'absolute',
+            top: '50%',
+            right: '10px',
+            transform: 'translateY(-50%)',
+            cursor: 'pointer',
+            zIndex: 1
+          }}
+        >
+          <Icon icon='ic:sharp-double-arrow' style={{ fontSize: 30, color: "rgb(87,39,0)" }} />
         </div>
+      </div>
     </div>
-  </div>
-
   );
 }
-
