8c7a001773919dc7b45745464209cb061b16ec18
Robert Marquez <legitem2023@gmail.com>
2025-05-19 11:35:27 +0800

    Update ReusableSlick.tsx

--- components/Reusable/ReusableSlick.tsx
+++ components/Reusable/ReusableSlick.tsx
@@ -19,7 +19,7 @@ const ReusableSlick = ({ data }) => {
         {data.map((item: any, idx: number) => (
           <SwiperSlide key={idx} style={{ backgroundColor: "transparent", textAlign:'left' }}>
             <img
-              style={{ borderRadius: '10px', margin: '10px',width:'80px',height:'80px' }}
+              style={{ borderRadius: '10px',width:'80px',height:'80px' }}
               height={200}
               width={200}
               src={item.image}
