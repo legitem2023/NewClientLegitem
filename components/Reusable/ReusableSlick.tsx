b2a0009acc4fd6db4539063db1020608bd574cc9
Robert Marquez <legitem2023@gmail.com>
2025-05-19 11:22:12 +0800

    Update ReusableSlick.tsx

--- components/Reusable/ReusableSlick.tsx
+++ components/Reusable/ReusableSlick.tsx
@@ -25,7 +25,7 @@ const ReusableSlick = ({ data }) => {
               src={item.image}
               alt={item.Name}
             />
-            <b style={{textAlign:'left',marginTop:'10px',fontSize:'13px'}}>{item.Name}</b>
+            <b style={{textAlign:'left',marginTop:'5px',fontSize:'12px'}}>{item.Name}</b>
           </SwiperSlide>
         ))}
       </Swiper>
