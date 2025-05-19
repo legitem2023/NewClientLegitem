70f9eb427f0898e6a3b7faaccf26d4a50dd41da8
Robert Marquez <legitem2023@gmail.com>
2025-05-19 09:59:20 +0800

    Update ReusableSlick.tsx

--- components/Reusable/ReusableSlick.tsx
+++ components/Reusable/ReusableSlick.tsx
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from "react";
 import Slider from "react-slick";
 import "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css";
@@ -42,3 +43,42 @@ function ReusableSlick() {
 }
 
 export default ReusableSlick;
+
+*/
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
+// import 'swiper/css/scrollbar';
+import ModelViewer from "../Partial/ThreeJS/ModelViewer";
+const ReusableSlick = () => {
+    
+    return (
+      <div style={{height:"auto",position:"relative"}}>
+            <Swiper
+            slidesPerView={4}
+            pagination={{
+              clickable: true,
+            }}
+            style={{position:"relative",width:"100%"}}
+            modules={[Pagination]}
+            loop={false}>
+              <SwiperSlide>
+                           <img height={150} width={150} src={"https://tsbriguuaznlvwbnylop.supabase.co/storage/v1/object/public/legitemfiles/ProductImages/Product-2025-1-1-12-15054.webp"} />
+              </SwiperSlide>
+                <SwiperSlide>
+                           <img height={150} width={150} src={"https://tsbriguuaznlvwbnylop.supabase.co/storage/v1/object/public/legitemfiles/ProductImages/Product-2025-1-1-12-15054.webp"} />
+              </SwiperSlide>
+              <SwiperSlide>
+                           <img height={150} width={150} src={"https://tsbriguuaznlvwbnylop.supabase.co/storage/v1/object/public/legitemfiles/ProductImages/Product-2025-1-1-12-15054.webp"} />
+              </SwiperSlide>
+              <SwiperSlide>
+                           <img height={150} width={150} src={"https://tsbriguuaznlvwbnylop.supabase.co/storage/v1/object/public/legitemfiles/ProductImages/Product-2025-1-1-12-15054.webp"} />
+              </SwiperSlide>
+          </Swiper>
+      </div>
+  )
+}
+
+export default ReusableSlick
