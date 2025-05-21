70f1dfe5fd26fbfe8ae3e36a4a05b5813dd57514
Robert Marquez <legitem2023@gmail.com>
2025-05-21 11:06:20 +0800

    Update ReusableSlideNames.tsx

--- components/Reusable/ReusableSlideNames.tsx
+++ components/Reusable/ReusableSlideNames.tsx
@@ -6,7 +6,7 @@ import 'swiper/css/pagination';
 import 'swiper/css';
 import Element_Title from '../UI/Element_Title';
 import ModelViewer from "../Partial/ThreeJS/ModelViewer";
-
+import CanvasTextImage from "../UI/CanvasTextImage";
 const ReusableSlideNames = ({ data }) => {
   return (
     <div className="card" style={{ height: "auto", position: "relative", padding: "10px" }}>
@@ -31,16 +31,7 @@ const ReusableSlideNames = ({ data }) => {
       >
         {data.map((item: any, idx: number) => (
           <SwiperSlide key={idx} style={{ backgroundColor: "transparent", textAlign:'left' }}>
-            <img
-              style={{
-                borderRadius: '5px',
-                width: '100%',
-                aspectRatio: '1 / 1',
-                objectFit: 'cover',
-              }}
-              src={item.image}
-              alt={item.Name}
-            />
+            <CanvasTextImage text={item.Name}/>
             <b style={{
               textAlign: 'left',
               marginTop: '10px',
@@ -50,7 +41,6 @@ const ReusableSlideNames = ({ data }) => {
               WebkitBoxOrient: 'vertical',
               overflow: 'hidden',
             }}>
-              {item.Name}
             </b>
           </SwiperSlide>
         ))}
