3befc45557bb5addc99b2f1bf1c8ac092acc66fd
Robert Marquez <legitem2023@gmail.com>
2025-05-21 16:24:31 +0800

    Update ReusableSlideNames.tsx

--- components/Reusable/ReusableSlideNames.tsx
+++ components/Reusable/ReusableSlideNames.tsx
@@ -1,29 +1,40 @@
 'use client'
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination,Autoplay} from 'swiper/modules';
+import { Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css/pagination';
 import 'swiper/css';
 import Element_Title from '../UI/Element_Title';
 import ModelViewer from "../Partial/ThreeJS/ModelViewer";
 import CanvasTextImage from "../UI/CanvasTextImage";
+
 const ReusableSlideNames = ({ data }) => {
   return (
-    <div className="card" style={{ width:"100%",aspectRatio:"10 / 2",position: "relative", padding: "10px" }}>
-      <Swiper
-        slidesPerView="auto"
-        spaceBetween={10}
-        autoplay={{delay:2500,disableOnInteraction:false}}
-        style={{ position: "relative", width: "100%",aspectRatio: "10 / 2" }}
-        loop={false}
-        modules={[Autoplay]}
-      >
-        {data.map((item: any, idx: number) => (
-          <SwiperSlide key={idx} style={{ backgroundColor: "red", textAlign:'left' , width: "100%",aspectRatio: "10 / 2" }}>
-            <CanvasTextImage text={item.Name}/>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div style={{ position: 'relative', width: '100%', paddingTop: '20%' }}>
+      <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+        <Swiper
+          slidesPerView="auto"
+          spaceBetween={10}
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          style={{ position: "relative", width: "100%", height: "100%" }}
+          loop={false}
+          modules={[Autoplay]}
+        >
+          {data.map((item: any, idx: number) => (
+            <SwiperSlide
+              key={idx}
+              style={{
+                backgroundColor: "red",
+                textAlign: 'left',
+                width: "100%",
+                height: "100%"
+              }}
+            >
+              <CanvasTextImage text={item.Name} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   )
 }
