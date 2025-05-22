3f5982f320ab607745153cb5df38cd06e468d32c
Robert Marquez <legitem2023@gmail.com>
2025-05-22 13:22:12 +0800

    Update ReusableSlickGrid.tsx

--- components/Reusable/ReusableSlickGrid.tsx
+++ components/Reusable/ReusableSlickGrid.tsx
@@ -1,16 +1,14 @@
 'use client'
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Pagination, Autoplay, Grid } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css/pagination';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
-import Element_Title from '../UI/Element_Title';
-import ModelViewer from "../Partial/ThreeJS/ModelViewer";
 
 const ReusableSlickGrid = ({ data }) => {
   return (
-    <div className="card" style={{ width: "100%",aspectRatio:'3 / 1', position: "relative", padding: "10px" }}>
+    <div className="card" style={{ width: "100%", position: "relative", padding: "10px" }}>
       <Swiper
         effect={'coverflow'}
         centeredSlides={true}
@@ -18,31 +16,41 @@ const ReusableSlickGrid = ({ data }) => {
         autoplay={{ delay: 2500, disableOnInteraction: false }}
         style={{ position: "relative", width: "100%" }}
         loop={false}
-        pagination={{
-          dynamicBullets:true
-        }}
+        pagination={{ dynamicBullets: true }}
         coverflowEffect={{
-          rotate:0,
-          stretch:10,
-          depth:100,
-          modifier:3.5,
-          slideShadows:true
+          rotate: 0,
+          stretch: 10,
+          depth: 100,
+          modifier: 3.5,
+          slideShadows: true,
         }}
-      
-        modules={[Autoplay,EffectCoverflow,Pagination]}
+        modules={[Autoplay, EffectCoverflow, Pagination]}
       >
         {data.map((item: any, idx: number) => (
-          <SwiperSlide key={idx} style={{ backgroundColor: "transparent", textAlign: 'left',width:'100%',aspectRatio:'4 / 3' }}>
-            <img
-              style={{
-                borderRadius: '5px',
-                width: '100%',
-                height:'100%',
-                objectFit: 'cover',
-              }}
-              src={item.image}
-              alt={item.Name}
-            />
+          <SwiperSlide
+            key={idx}
+            style={{
+              backgroundColor: "transparent",
+              textAlign: 'left',
+              width: '100%',
+            }}
+          >
+            <div style={{
+              width: '100%',
+              aspectRatio: '16 / 9', // Change to '4 / 3' if needed
+              overflow: 'hidden',
+              borderRadius: '5px',
+            }}>
+              <img
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }}
+                src={item.image}
+                alt={item.Name}
+              />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
