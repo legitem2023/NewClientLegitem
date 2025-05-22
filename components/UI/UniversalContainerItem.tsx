4ce987ca96bc303b22759db782ba6464fec662d0
Robert Marquez <legitem2023@gmail.com>
2025-05-22 22:55:29 +0800

    Update UniversalContainerItem.tsx

--- components/UI/UniversalContainerItem.tsx
+++ components/UI/UniversalContainerItem.tsx
@@ -4,7 +4,7 @@ import HtmlRenderer from 'components/Html/HtmlRenderer';
 import TimestampConverter from 'components/Partial/timestamp/TimestampConverter';
 import Image from 'next/image';
 import { imageSource } from 'utils/scripts';
-
+import Element_Title from './Element_Title';
 interface UniversalContainerItemProps {
   title: string;
   thumbnail: string;
@@ -21,6 +21,7 @@ const UniversalContainerItem: React.FC<UniversalContainerItemProps> = ({ title, 
         <Image src={imageSource(thumbnail)} height={100} width={200} alt={`image${index}`} />
       </div>
       <div className="ReusableCardContainer_1">
+        <Element_Title Label="" value={title}/>
         <HtmlRenderer htmlContent={summary} />
         <TimestampConverter timestamp={dateCreated} />
       </div>
