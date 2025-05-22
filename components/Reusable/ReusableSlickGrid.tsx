721d48ede18b900592fefef5475192916bb878fb
Robert Marquez <legitem2023@gmail.com>
2025-05-22 08:00:35 +0800

    Update ReusableSlickGrid.tsx

--- components/Reusable/ReusableSlickGrid.tsx
+++ components/Reusable/ReusableSlickGrid.tsx
@@ -13,7 +13,7 @@ const ReusableSlickGrid = ({ data }) => {
     <div className="card" style={{ height: "auto", position: "relative", padding: "10px" }}>
       <Swiper
         effect={'coverflow'}
-        slidesPerView={3}
+        slidesPerView={2}
         
         centeredSlides={true}
         autoplay={{ delay: 2500, disableOnInteraction: false }}
