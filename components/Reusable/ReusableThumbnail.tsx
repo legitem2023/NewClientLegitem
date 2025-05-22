ea621d57e89e4f91664174de92a286eba48bd5b1
Robert Marquez <legitem2023@gmail.com>
2025-05-22 23:53:59 +0800

    Update ReusableThumbnail.tsx

--- components/Reusable/ReusableThumbnail.tsx
+++ components/Reusable/ReusableThumbnail.tsx
@@ -38,7 +38,7 @@ const ReusableThumbnail: React.FC<ReusableThumbnailProps> = ({
         {item.model===null?"":(<Optional3D/>)}
           <Image
             src={imageSource(item)}
-            style={{filter:item.stock < 1?'grayscale(100%) blur(3px)':''}}
+            style={{filter:item.stock < 1?'grayscale(100%)':''}}
             loading='lazy'
             height="200"
             width="200"
