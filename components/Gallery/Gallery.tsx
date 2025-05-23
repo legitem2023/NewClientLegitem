d3e80f2925a1dbec38c3e9449d6260292debd2ca
Robert Marquez <legitem2023@gmail.com>
2025-05-23 14:06:55 +0800

    Update Gallery.tsx

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -1,3 +1,14 @@
+import React from 'react'; 
+import { Swiper, SwiperSlide } from 'swiper/react'; 
+import { Pagination } from 'swiper/modules'; 
+import 'swiper/css/pagination'; 
+import 'swiper/css'; 
+import 'swiper/css/scrollbar'; 
+import Image from 'next/image'; 
+import { handleError, imageSourceGallery } from 'utils/scripts'; 
+import { useSelector } from 'react-redux';
+
+type PropsGallery = { data: (item: string) => void; length:any, slidesPerView:number, spaceBetween:number }
 export const Gallery:React.FC<PropsGallery> = ({data,length,slidesPerView,spaceBetween}:any) => {
   const imgPath = process.env.NEXT_PUBLIC_SERVER_PRODUCT_IMAGE_PATH || '';
   const viewedID = useSelector((state:any) => state.viewed.viewed); // Access category state
