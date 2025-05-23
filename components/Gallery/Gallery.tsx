1bcfadaa337f22745e086f1d60518dc8b36243be
Robert Marquez <legitem2023@gmail.com>
2025-05-23 13:55:30 +0800

    Update Gallery.tsx

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -10,47 +10,55 @@ import { useSelector } from 'react-redux';
 
 type PropsGallery = {
   data: (item: string) => void;
-  length:any,
-  slidesPerView:number,
-  spaceBetween:number
-}
+  length: any;
+  slidesPerView: number;
+  spaceBetween: number;
+};
 
-export const Gallery:React.FC<PropsGallery> = ({data,length,slidesPerView,spaceBetween}:any) => {
+export const Gallery: React.FC<PropsGallery> = ({ data, length, slidesPerView, spaceBetween }: any) => {
   const imgPath = process.env.NEXT_PUBLIC_SERVER_PRODUCT_IMAGE_PATH || '';
-  const viewedID = useSelector((state:any) => state.viewed.viewed); // Access category state
+  const viewedID = useSelector((state: any) => state.viewed.viewed);
 
-  const initialSlideIndex = data.subImageFieldOut.findIndex((img) => img.ImagePath === viewedID);
-  
-  
+  const initialSlideIndex = data.subImageFieldOut.findIndex((img: any) => img.ImagePath === viewedID);
+
   return (
-      <Swiper
+    <Swiper
       spaceBetween={spaceBetween}
       slidesPerView={slidesPerView}
       initialSlide={initialSlideIndex !== -1 ? initialSlideIndex : 0}
-      pagination={{
-        clickable: true,
-      }}
+      pagination={{ clickable: true }}
       modules={[Pagination]}
-      loop={true}>
-      {length.subImageFieldOut.length > 0?
-        data.subImageFieldOut.map((item:any, i:any) => (
-        <SwiperSlide key={i}>
-            <Image key={i} 
-                   src={imageSourceGallery(item)} 
-                   onError={handleError}
-                   alt={"alt" + i} 
-                   style={{aspectRatio:'16 / 9', width:'100%'}}
-                   width='1600' height='900'/>
+      loop
+      style={{
+        aspectRatio: '16 / 9',
+      }}
+    >
+      {length.subImageFieldOut.length > 0 ? (
+        data.subImageFieldOut.map((item: any, i: number) => (
+          <SwiperSlide key={i} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Image
+              src={imageSourceGallery(item)}
+              onError={handleError}
+              alt={`alt${i}`}
+              style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+              width={1600}
+              height={900}
+            />
             {item.title}
+          </SwiperSlide>
+        ))
+      ) : (
+        <SwiperSlide style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Image
+            src={`${imgPath}`}
+            onError={handleError}
+            alt="alt1"
+            style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+            width={1600}
+            height={900}
+          />
         </SwiperSlide>
-      )):<SwiperSlide>
-      <Image src={`${imgPath}`} 
-             onError={handleError}
-             alt={"alt" + 1} 
-             style={{aspectRatio:'16 / 9', width:'100%'}}
-             width='1600' height='900'/>
-        </SwiperSlide>
-      }
+      )}
     </Swiper>
   );
 };
