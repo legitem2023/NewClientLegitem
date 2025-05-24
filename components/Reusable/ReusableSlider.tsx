b0a024bbd8b88209c45307c6b26100af06433e79
Robert Marquez <legitem2023@gmail.com>
2025-05-24 23:07:21 +0800

    Update ReusableSlider.tsx

--- components/Reusable/ReusableSlider.tsx
+++ components/Reusable/ReusableSlider.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { FreeMode, Navigation } from 'swiper/modules'
@@ -14,19 +13,12 @@ interface ReusableSliderProps {
 const ReusableSlider: React.FC<ReusableSliderProps> = ({ items }) => {
   return (
     <div className="searchContainer" style={{ width: '100%', backgroundColor: '#f1f1f1', overflow: 'hidden' }}>
-      <Swiper
-        slidesPerView={2}
-        spaceBetween={5}
-        navigation={true}
-        freeMode={false}
-        modules={[FreeMode, Navigation]}
-        style={{ padding: 0, width: '100%', height: '100%', boxShadow: '0.5px 0.5px 3px #000000', backgroundColor: 'purple' }}
-        
-      >
+      <Swiper slidesPerView={2} spaceBetween={5} navigation={true} freeMode={false} modules={[FreeMode, Navigation]}
+        style={{ padding: 0, width: '100%', height: '100%' }}>
         {items.map((item, index) => (
-          <SwiperSlide key={index} style={{ backgroundColor: 'red' }}>
-            <div style={{ width: '100%', height: '100%', backgroundColor: 'green' }}>
-              
+          <SwiperSlide key={index}>
+            <div style={{ width: '100%', height: '100%' }}>
+              {item}
             </div>
           </SwiperSlide>
         ))}
