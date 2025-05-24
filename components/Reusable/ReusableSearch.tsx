03e513a0f7a367687925042411b6a18fbd7d7123
Robert Marquez <legitem2023@gmail.com>
2025-05-24 17:39:05 +0800

    Update ReusableSearch.tsx

--- components/Reusable/ReusableSearch.tsx
+++ components/Reusable/ReusableSearch.tsx
@@ -1,29 +1,39 @@
 import { Icon } from '@iconify/react';
-import React, { FC, ReactNode } from 'react'
-type ReusableSearchProps = {
-    search:(event: React.SyntheticEvent<HTMLImageElement, Event>) => void;
-    sort:(event: React.SyntheticEvent<HTMLImageElement, Event>) => void;
-    trigger:(event: React.SyntheticEvent<HTMLImageElement, Event>) => void;
-}
-const ReusableSearch:FC<ReusableSearchProps> = ({search,sort,trigger}) => {
-  return (
-        <div className='searchContaier'>
-            {/* <div><input type='text' placeholder='Search' onChange={(e: any) => search(e)}></input></div> */}
-            <div>
-            <label htmlFor="mySelect" className='hidden'>Choose an option:</label>
-            <select onChange={(e:any) => sort(e)}>
-                <option value=''>Sort</option>
-                <option value='name'>By Name</option>
-                <option value='price'>By Price</option>
-            </select>
-            </div>
-            <div>
-                <button onClick={()=>trigger} aria-label='sort'>
-                <Icon icon="bx:sort" />
-                </button>
-            </div>
-        </div>
-  )
-}
+import React, { FC } from 'react';
+import ReusableSlider from './ReusableSlider';
 
-export default ReusableSearch
+type ReusableSearchProps = {
+  search: (event: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+  sort: (event: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+  trigger: (event: React.SyntheticEvent<HTMLImageElement, Event>) => void;
+};
+
+const ReusableSearch: FC<ReusableSearchProps> = ({ search, sort, trigger }) => {
+  const items: React.ReactNode[] = [
+    (
+      <div>
+        <label htmlFor="mySelect" className="hidden">Choose an option:</label>
+        <select onChange={(e: any) => sort(e)}>
+          <option value="">Sort</option>
+          <option value="name">By Name</option>
+          <option value="price">By Price</option>
+        </select>
+      </div>
+    ),
+    (
+      <div>
+        <button onClick={(e) => trigger(e)} aria-label="sort">
+          <Icon icon="bx:sort" />
+        </button>
+      </div>
+    )
+  ];
+
+  return (
+    <div className="searchContaier">
+      <ReusableSlider item={items} />
+    </div>
+  );
+};
+
+export default ReusableSearch;
