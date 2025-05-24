45104ba8e015012aa0a5938ea8db44962cf5f779
Robert Marquez <legitem2023@gmail.com>
2025-05-24 19:32:43 +0800

    Update ReusableSlider.tsx

--- components/Reusable/ReusableSlider.tsx
+++ components/Reusable/ReusableSlider.tsx
@@ -12,13 +12,13 @@ interface ReusableSliderProps {
 
 const ReusableSlider: React.FC<ReusableSliderProps> = ({ items }) => {
   return (
-    <div className="searchContaier" style={{width:'100%'}}>
+    <div className="searchContaier" style={{width:'100%',backgroundColor:'red'}}>
       <Swiper
         slidesPerView={3}
         freeMode={true}
         modules={[FreeMode]}
         spaceBetween={0}
-        style={{ padding: 0,width:'100%' }}
+        style={{ padding: 0,width:'100%',boxShadow:'0.5px 0.5px 3px #000000' }}
       >
         {items.map((item, index) => (
           <SwiperSlide key={index}>
