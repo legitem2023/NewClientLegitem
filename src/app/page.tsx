d6ca0a60d757a2bcacd0f38e3478f3e28efbebd9
Robert Marquez <legitem2023@gmail.com>
2025-06-23 03:00:10 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import PageBody from '../../components/Products/ProductsBody';
 import HomeBody from '../../components/Home/HomeBody'
 import { Suspense } from 'react';
 import Loading from 'components/Partial/LoadingAnimation/Loading';
+import ReusableSwiperTabs from 'components/Layout/ReusableSwiperTabs';
 export default function Index() {
+  const tabItems = [
+    { name: 'Docs', icon: '📄', content: <HomeBody /> },
+    { name: 'Store', icon: '🛒', content: <HomeBody /> },
+    { name: 'Settings', icon: '⚙️', content: <HomeBody /> }
+  ];
   return (
     <Suspense fallback={<Loading/>}>
     <div className='Main'>
@@ -12,6 +18,7 @@ export default function Index() {
       <HomeBody/>
       <PageFooter />
     </div>
+      <ReusableSwiperTabs tabs={tabItems}/>
     </Suspense>
   )
 }
