5bbec8eb7ffc99f2f189bbc3922ffc80905678de
Robert Marquez <legitem2023@gmail.com>
2025-06-23 22:14:17 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ import {
 
 //import useOrderStatusNotification from 'components/Hooks/useOrderStatusNotification';
 //import ReusableArrowTabs from 'components/Reusable/ReusableArrowTabs';
-//import PageOrder from 'components/Order/PageOrder';
+import Order from 'components/Order/Order';
 //import PageAccount from 'components/Account/PageAccount';
 import Products from 'components/Products/Products';
 import News from 'components/News/News';
@@ -32,17 +32,17 @@ import Home from 'components/Home/Home';
 import Loading from 'components/Partial/LoadingAnimation/Loading';
 import ReusableSwiperTabs from 'components/Layout/ReusableSwiperTabs';
 import ReusableTabs from 'components/Reusable/ReusableTabs';
-import { cookies } from 'components/cookies/cookie';
-import { setorderStage } from 'Redux/orderStageSlice';
-import { ClearStorage } from 'utils/scripts';
+//import { cookies } from 'components/cookies/cookie';
+//import { setorderStage } from 'Redux/orderStageSlice';
+//import { ClearStorage } from 'utils/scripts';
 
 export default function Index() {
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const saved_cookie = useSelector((state: any) => state.cookie.cookie);
-  const cookieEmailAddress = saved_cookie?.emailAddress;
+  //const router = useRouter();
+  //const dispatch = useDispatch();
+  //const saved_cookie = useSelector((state: any) => state.cookie.cookie);
+  //const cookieEmailAddress = saved_cookie?.emailAddress;
   //const CurrentOrderStage = useSelector((state: any) => state.orderStage.orderStage);
 
   const tabItms = [
@@ -58,7 +58,7 @@ export default function Index() {
     { name: 'Products', icon: 'bi:tags-fill', content: <Products /> },
     { name: 'News', icon: 'fa6-solid:newspaper', content: <News /> },
     { name: 'Crowd', icon: 'simple-icons:crowdsource', content: <Messages /> },
-    { name: 'Account', icon: 'mdi:badge-account-horizontal', content: <ReusableTabs tabs={tabItms} /> },
+    { name: 'Account', icon: 'mdi:badge-account-horizontal', content: <Order/> },
     { name: 'Cart', icon: 'mdi:cart', content: <CartBody /> },
   ];
 
