614aed5fbca892b44923aea3c3e3851a3e529951
Robert Marquez <legitem2023@gmail.com>
2025-06-23 22:06:52 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState, Suspense } from 'react';
 import { useQuery } from '@apollo/client';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRouter } from 'next/navigation';
-
+/*
 import {
   READ_ORDERS,
   READ_ORDERS_RECIEVED,
@@ -12,18 +12,18 @@ import {
   READ_ORDERS_DELIVER,
   READ_ORDERS_DELIVERED,
 } from 'graphql/queries';
+*/
+//import AccordionOrders from 'components/AccordionOrders/AccordionOrders';
+//import AccordionOrderRecieved from 'components/AccordionOrders/AccordionOrderRecieved';
+//import AccordionOrderPacked from 'components/AccordionOrders/AccordionOrderPacked';
+//import AccordionOrderLogistic from 'components/AccordionOrders/AccordionOrderLogistic';
+//import AccordionOrderDeliver from 'components/AccordionOrders/AccordionOrderDeliver';
+//import AccordionOrderDelivered from 'components/AccordionOrders/AccordionOrderDelivered';
 
-import AccordionOrders from 'components/AccordionOrders/AccordionOrders';
-import AccordionOrderRecieved from 'components/AccordionOrders/AccordionOrderRecieved';
-import AccordionOrderPacked from 'components/AccordionOrders/AccordionOrderPacked';
-import AccordionOrderLogistic from 'components/AccordionOrders/AccordionOrderLogistic';
-import AccordionOrderDeliver from 'components/AccordionOrders/AccordionOrderDeliver';
-import AccordionOrderDelivered from 'components/AccordionOrders/AccordionOrderDelivered';
-
-import useOrderStatusNotification from 'components/Hooks/useOrderStatusNotification';
-import ReusableArrowTabs from 'components/Reusable/ReusableArrowTabs';
-import PageOrder from 'components/Order/PageOrder';
-import PageAccount from 'components/Account/PageAccount';
+//import useOrderStatusNotification from 'components/Hooks/useOrderStatusNotification';
+//import ReusableArrowTabs from 'components/Reusable/ReusableArrowTabs';
+//import PageOrder from 'components/Order/PageOrder';
+//import PageAccount from 'components/Account/PageAccount';
 import Products from 'components/Products/Products';
 import News from 'components/News/News';
 import CartBody from 'components/Cart/CartBody';
@@ -47,7 +47,7 @@ export default function Index() {
 
   const tabItms = [
     { name: 'Address Book', icon: 'ic:sharp-home', content: <PageAccount userId={saved_cookie.userid} /> },
-    { name: 'My Orders', icon: 'bi:tags-fill', content: </> },
+    { name: 'My Orders', icon: 'bi:tags-fill', content: <></> },
     { name: 'Likes', icon: 'fa6-solid:newspaper', content: <News /> },
     { name: 'Messages', icon: 'simple-icons:crowdsource', content: <Messages /> },
     { name: 'Logout', icon: 'mdi:badge-account-horizontal', content: <CartBody /> },
