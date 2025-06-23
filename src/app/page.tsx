5e13fb353e00c51e229268a767ff231b9f4e7d1b
Robert Marquez <legitem2023@gmail.com>
2025-06-23 22:18:33 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,8 @@ import React, { useEffect, useState, Suspense } from 'react';
 import { useQuery } from '@apollo/client';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRouter } from 'next/navigation';
-/*
-import {
-  READ_ORDERS,
-  READ_ORDERS_RECIEVED,
-  READ_ORDERS_PACKED,
-  READ_ORDERS_LOGISTIC,
-  READ_ORDERS_DELIVER,
-  READ_ORDERS_DELIVERED,
-} from 'graphql/queries';
-*/
-//import AccordionOrders from 'components/AccordionOrders/AccordionOrders';
-//import AccordionOrderRecieved from 'components/AccordionOrders/AccordionOrderRecieved';
-//import AccordionOrderPacked from 'components/AccordionOrders/AccordionOrderPacked';
-//import AccordionOrderLogistic from 'components/AccordionOrders/AccordionOrderLogistic';
-//import AccordionOrderDeliver from 'components/AccordionOrders/AccordionOrderDeliver';
-//import AccordionOrderDelivered from 'components/AccordionOrders/AccordionOrderDelivered';
 
-//import useOrderStatusNotification from 'components/Hooks/useOrderStatusNotification';
-//import ReusableArrowTabs from 'components/Reusable/ReusableArrowTabs';
+import ReusableArrowTabs from 'components/Reusable/ReusableArrowTabs';
 import Order from 'components/Order/Order';
 //import PageAccount from 'components/Account/PageAccount';
 import Products from 'components/Products/Products';
@@ -32,22 +15,14 @@ import Home from 'components/Home/Home';
 import Loading from 'components/Partial/LoadingAnimation/Loading';
 import ReusableSwiperTabs from 'components/Layout/ReusableSwiperTabs';
 import ReusableTabs from 'components/Reusable/ReusableTabs';
-//import { cookies } from 'components/cookies/cookie';
-//import { setorderStage } from 'Redux/orderStageSlice';
-//import { ClearStorage } from 'utils/scripts';
 
 export default function Index() {
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  //const router = useRouter();
-  //const dispatch = useDispatch();
-  //const saved_cookie = useSelector((state: any) => state.cookie.cookie);
-  //const cookieEmailAddress = saved_cookie?.emailAddress;
-  //const CurrentOrderStage = useSelector((state: any) => state.orderStage.orderStage);
 
   const tabItms = [
-    { name: 'Address Book', icon: 'ic:sharp-home', content: <PageAccount userId={saved_cookie.userid} /> },
-    { name: 'My Orders', icon: 'bi:tags-fill', content: <></> },
+    { name: 'Address Book', icon: 'ic:sharp-home', content: <Order/> },
+    { name: 'My Orders', icon: 'bi:tags-fill', content: <Order/> },
     { name: 'Likes', icon: 'fa6-solid:newspaper', content: <News /> },
     { name: 'Messages', icon: 'simple-icons:crowdsource', content: <Messages /> },
     { name: 'Logout', icon: 'mdi:badge-account-horizontal', content: <CartBody /> },
