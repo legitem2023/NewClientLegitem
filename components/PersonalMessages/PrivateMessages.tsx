54f8acfcc4bca05aa28b9cadc36bf0f9091b0fb7
Robert Marquez <legitem2023@gmail.com>
2025-06-25 01:52:11 +0800

    Update PrivateMessages.tsx

--- components/PersonalMessages/PrivateMessages.tsx
+++ components/PersonalMessages/PrivateMessages.tsx
@@ -7,6 +7,11 @@ import { useSelector } from 'react-redux'
 import ReusableSwipeMenu from 'components/Reusable/ReusableSwipeMenu';
 const PrivateMessages = () => {
     const cookie = useSelector((state:any)=> state.cookie.cookie);
+    const menu = [{ label: "Dashboard", href: "/" },
+  { label: "Profile", href: "/profile" },
+  { label: "Settings", href: "/settings" },
+  { label: "Logout", href: "/logout" },
+];
     const SelectedReciever = useSelector((state:any)=> state.reciever.reciever);
     return (
         <ReusableSwipeMenu menuItems={menu} 
