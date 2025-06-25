fe759d9a135fff125ffbb9d545e05653b28ba140
Robert Marquez <legitem2023@gmail.com>
2025-06-25 20:26:08 +0800

    Update LikesTab.tsx

--- components/Likes/LikesTab.tsx
+++ components/Likes/LikesTab.tsx
@@ -1,1 +1,14 @@
-k
+'use client'
+import React from 'react'
+import { Icon } from '@iconify/react'
+import AccountMenu from 'components/Account/AccountMenu'
+import LikesData from './LikesData'
+import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
+
+const LikesTab:React.FC = () => {
+  return (
+    <LikesData/>
+  )
+}
+
+export default LikesTab
