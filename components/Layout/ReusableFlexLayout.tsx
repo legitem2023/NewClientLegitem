f176d2a3fcaafbec537e2d7206246688c2a5119e
Robert Marquez <legitem2023@gmail.com>
2025-06-26 23:47:58 +0800

    Update ReusableFlexLayout.tsx

--- components/Layout/ReusableFlexLayout.tsx
+++ components/Layout/ReusableFlexLayout.tsx
@@ -10,7 +10,7 @@ const ReusableFlexLayout:FC<ReusableMainLayoutProps> = ({childA,childB,childC}) 
       <div className="LeftContainer">
         {childA()}
       </div>
-      <div className='CentralContainer'>
+      <div className='CentralContainer' style={{position:"absolute"}}>
         {childB()}
       </div>
       <div className='RightContainer'>
