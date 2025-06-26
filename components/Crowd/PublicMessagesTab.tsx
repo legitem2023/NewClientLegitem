4e57ba068e46f4886a7189258efcc15f6f65c7d4
Robert Marquez <legitem2023@gmail.com>
2025-06-26 21:38:47 +0800

    Update and rename CrowdMessagesTab.tsx to PublicMessagesTab.tsx

---
+++ components/Crowd/PublicMessagesTab.tsx
@@ -0,0 +1,24 @@
+
+'use client'
+import React from 'react'
+import Messages from './Messages'
+import ActiveUsers from './ActiveUsers'
+import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
+import { useSelector } from 'react-redux'
+import ReusableSwipeMenu from 'components/Reusable/ReusableSwipeMenu';
+const PublicMessagesTab = () => {
+    const cookie = useSelector((state:any)=> state.cookie.cookie);
+    const menu = [{ label: "Dashboard", href: "/" },
+  { label: "Profile", href: "/profile" },
+  { label: "Settings", href: "/settings" },
+  { label: "Logout", href: "/logout" },
+];
+    const SelectedReciever = useSelector((state:any)=> state.reciever.reciever);
+    return (
+        <ReusableSwipeMenu menuItems={menu} 
+                           menu={()=>(<ActiveUsers email={SelectedReciever}/>)} 
+                           main={()=>(<Messages/>)}/>
+    )
+}
+
+export default PublicMessagesTab
