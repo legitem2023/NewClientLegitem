f1284cd2349b117b22234a8eddcba2b9a58493e1
Robert Marquez <legitem2023@gmail.com>
2025-06-26 15:49:11 +0800

    Create ReusableFlexLayout.tsx

---
+++ components/Layout/ReusableFlexLayout.tsx
@@ -0,0 +1,23 @@
+import { ReusableMainLayoutProps } from '@/types';
+import React, { FC, ReactNode } from 'react'
+import { useSelector } from 'react-redux';
+
+
+const ReusableFlexLayout:FC<ReusableMainLayoutProps> = ({childA,childB,childC}) => {
+    const drawerState = useSelector((state:any)=> state.drawer.drawer)
+    return (
+    <div className=''>
+      <div className="LeftContainer">
+        {childA()}
+      </div>
+      <div className='CentralContainer'>
+        {childB()}
+      </div>
+      <div className='RightContainer'>
+        {childC()}
+      </div>
+    </div>
+  )
+}
+
+export default ReusableFlexLayout
