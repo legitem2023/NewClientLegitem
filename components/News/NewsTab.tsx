471a2813da1de50904c7a282a6f50fa8610ad4e6
Robert Marquez <legitem2023@gmail.com>
2025-06-26 21:26:03 +0800

    Update NewsTab.tsx

--- components/News/NewsTab.tsx
+++ components/News/NewsTab.tsx
@@ -1,1 +1,13 @@
 import ReusableFlexLayout from 'components/Layout/ReusableFlexLayout'
+import News from 'components/News/News';
+import React from 'react';
+const NewsTab:React.FC = () => {
+  return (
+    <ReusableFlexLayout 
+      childA={()=>(<></>)}
+      childB={()=>(<News/>)} 
+      childC={()=>(<></>)}/>
+  );
+};
+
+export default NewsTab;
