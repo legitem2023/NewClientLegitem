f6c1adcfe61813c323f765a40874e005849d7e26
Robert Marquez <legitem2023@gmail.com>
2025-06-26 22:36:02 +0800

    Create LoginTab.tsx

---
+++ components/Login/LoginTab.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Menu from 'components/Partial/Menu'
+import React from 'react'
+//import { ViewGallery } from 'components/Gallery/ViewGallery'
+// import { useGlobalState } from 'state'
+//import { Icon } from '@iconify/react'
+//import Carousel from 'components/Carousel'
+//import Commercial3DModel from 'components/Partial/ThreeJS/Commercial3DModel'
+//import ThreeJS from 'components/Partial/ThreeJS/ThreeJS'
+//import Loading from 'components/Partial/LoadingAnimation/Loading'
+//import { useQuery } from '@apollo/client'
+//import { GET_CATEGORY } from 'graphql/queries'
+import { useSelector } from 'react-redux'
+import ReusableFlexLayout from 'components/Layout/ReusableFlexLayout'
+import Login from './Login'
+import ReusableSlideNames from 'components/Reusable/ReusableSlideNames';
+
+const LoginTab:React.FC = () => {
+const storeproductType = useSelector((state: any) => state.productTypeData.productTypeData);
+  return (
+     <ReusableFlexLayout 
+     childA={()=>(<ReusableSlideNames data={storeproductType} />)} 
+     childB={()=>(<Login/>)}
+     childC={()=>(<ReusableSlideNames data={storeproductType} />)}/>
+  )
+}
+
+export default LoginTab
