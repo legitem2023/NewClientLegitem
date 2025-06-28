089a96bf388f0e7523ab8aef8be31ec1a3026a6f
Robert Marquez <legitem2023@gmail.com>
2025-06-28 09:41:57 +0800

    Update LikesTab.tsx

--- components/Likes/LikesTab.tsx
+++ components/Likes/LikesTab.tsx
@@ -1,14 +1,62 @@
-'use client'
-import React from 'react'
-import { Icon } from '@iconify/react'
-import AccountMenu from 'components/Account/AccountMenu'
-import LikesData from './LikesData'
-import ReusableMainLayout from 'components/Layout/ReusableMainLayout'
+import React, { useEffect, useCallback, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { useSelector, useDispatch } from 'react-redux';
 
-const LikesTab:React.FC = () => {
+import ReusableCenterLayout from 'components/Layout/ReusableCenterLayout';
+import LikesLoading from './LikesLoading';
+import ReusableServerDown from 'components/Reusable/ReusableServerDown';
+import ReusableLikeCard from 'components/Reusable/ReusableLikeCard';
+import { READ_LIKES } from 'graphql/queries';
+import { setViewedProd } from 'Redux/viewedProdSlice';
+import { imageSource } from 'utils/scripts';
+import AddCartCmd from 'components/Commands/AddCartCmd';
+const LikesTab = () => {
+  const dispatch = useDispatch();
+  const cookie = useSelector((state: any) => state.cookie.cookie);
+  const [useLoading, setLoading] = useState(false);
+
+  const fallbackImage = `https://hokei-storage.s3.ap-northeast-1.amazonaws.com/images/Legit/IconImages/Legitem-svg.svg`;
+
+  const handleError = useCallback(
+    (event: React.SyntheticEvent<HTMLImageElement>) => {
+      event.currentTarget.src = fallbackImage;
+      event.currentTarget.srcset = fallbackImage;
+    },
+    [fallbackImage]
+  );
+
+  const handleLoading = useCallback(() => {
+    // Optional: add logic here
+  }, []);
+
+  const view = (item: any) => {
+   // dispatch(setViewedProd([item]));
+  };
+
+  const { data: LikeData, loading: LikeLoading, error: LikeError } = useQuery(READ_LIKES, {
+    variables: {
+      accountEmail: cookie.emailAddress,
+    },
+  });
+
+  if (LikeLoading) return <LikesLoading/>;
+  if (LikeError) return <ReusableServerDown />;
   return (
-    <LikesData/>
-  )
-}
+      <div style={{ overflowY: 'auto', height: 'auto', scrollbarWidth: 'none' }}>
+        <div className="LikeContainer">
+          {LikeData?.readLikes?.length > 0 ? (
+            LikeData.readLikes.map((item: any, idx: number) => (
+              <ReusableLikeCard
+                key={idx}
+                item={item}
+                view={() => view(item)}
+                imageSource={() => imageSource(item)}
+                handleError={handleError}
+                handleLoading={handleLoading}
+                childA={()=>(<AddCartCmd item={item}/>)} />
+            )
+          )):null}</div>
+      </div>
+    )
 
-export default LikesTab
+export default LikesTab;
