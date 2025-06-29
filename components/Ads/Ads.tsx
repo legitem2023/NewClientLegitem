fb215dbd0c5f385bb8f60ca1c160a9f7ce02d4ff
Robert Marquez <legitem2023@gmail.com>
2025-06-29 13:31:58 +0800

    Update Ads.tsx

--- components/Ads/Ads.tsx
+++ components/Ads/Ads.tsx
@@ -1,1 +1,12 @@
+'use client'
+import ReusableSlideNames from 'components/Reusable/ReusableSlideNames';
+const Ads:React.FC = () => {
+//const storeproductType = useSelector((state: any) => state.productTypeData.productTypeData);
+  const Ads = [{
+    Name:'Showcase your business in this space! Message: 09153392813'}]
+  return (
+     <ReusableSlideNames data={Ads} />)}/>
+  )
+}
 
+export default Ads
