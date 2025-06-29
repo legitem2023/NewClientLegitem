71c3ded514930513d7ef8d7d7921dadfbd66d46e
Robert Marquez <legitem2023@gmail.com>
2025-06-29 11:49:06 +0800

    Create SmoothScrollPolyfill.tsx

---
+++ components/Partial/Header/SmoothScrollPolyfill.tsx
@@ -0,0 +1,16 @@
+// components/SmoothScrollPolyfill.tsx
+'use client';
+
+import { useEffect } from 'react';
+
+export default function SmoothScrollPolyfill() {
+  useEffect(() => {
+    if ('scrollBehavior' in document.documentElement.style === false) {
+      import('smoothscroll-polyfill').then((module) => {
+        module.polyfill();
+      });
+    }
+  }, []);
+
+  return null; // This component doesn't render anything
+}
