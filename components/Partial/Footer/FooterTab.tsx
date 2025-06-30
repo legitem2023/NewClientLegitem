5109bf576f81fc01fbdf0f72ad6c7307c9623741
Robert Marquez <legitem2023@gmail.com>
2025-06-30 19:56:18 +0800

    Create FooterTab.tsx

---
+++ components/Partial/Footer/FooterTab.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { Icon } from '@iconify/react'
+import React, { useState } from 'react'
+import Link from 'next/link'
+import {useSelector} from 'react-redux';
+const FooterTab:React.FC = () => {
+  
+  return (
+    <div className='footer'>
+      <div className='FootHeader'>
+        <Icon icon="logos:tiktok-icon" />
+        <Icon icon="entypo-social:facebook" style={{color:'#104291'}}/>
+        <Icon icon="entypo-social:instagram" style={{color:'#d609ad'}}/>
+        <Icon icon="entypo-social:youtube" style={{color:'#ff0000'}}/>
+      </div>
+      <div className='FootCenter'>
+      <span className='foot_label'>
+        About Legitem
+      </span>
+      <span className='foot_label'>
+        FAQ
+      </span>
+      <span className='foot_label'>
+        Disclaimer
+      </span>
+      <span className='foot_label'>
+        Privacy
+      </span>
+      <span className='foot_label'>
+        Contact Us
+      </span>
+      </div>
+      <div className='FootFooter'>
+        All Right Reserved ©2024
+      </div>
+    </div>
+  )
+}
+
+export default FooterTab
