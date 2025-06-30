83fe5f09f925a4de43a291fb907c922e31f2c2c7
Robert Marquez <legitem2023@gmail.com>
2025-06-30 16:10:43 +0800

    Update LikesLoading.tsx

--- components/Likes/LikesLoading.tsx
+++ components/Likes/LikesLoading.tsx
@@ -15,7 +15,10 @@ const LikesLoading = () => {
         >
          <div className="LikeContainer">
            {[...Array(10)].map((_, index) => (
-             <div key={index} className='loading-screen' style={{height:130,marginTop:"3px",marginBottom:"3px"}}></div>
+             <div key={index} className='loading-screen' style={{ aspectRatio:'1/1',
+                                                                  width:'100%',
+                                                                  marginTop:"3px", 
+                                                                  marginBottom:"3px"}}></div>
            ))}
          </div>
         </div>
