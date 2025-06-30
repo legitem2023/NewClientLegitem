60371ba881ebcd2e03ead183ae21f4eae277b8e8
Robert Marquez <legitem2023@gmail.com>
2025-06-30 18:00:24 +0800

    Update Ads.tsx

--- components/Ads/Ads.tsx
+++ components/Ads/Ads.tsx
@@ -1,9 +1,10 @@
 'use client'
 import ReusableSlideNames from 'components/Reusable/ReusableSlideNames';
+import marquee from from 'components/UI/Marquee';
 import AdDropdown from './AdDropdown';
 const Ads:React.FC = () => {
 
-  const Dropdown = (<marquee style={{transition:'ease 0.5s',height:'80px'}}>Spotted! The perfect place for your products call now 09153392813</marquee>)
+  const Dropdown = (<marquee >Spotted! The perfect place for your products call now 09153392813</marquee>)
   return (
      <AdDropdown content={Dropdown}/>
   )
