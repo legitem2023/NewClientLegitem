070295699184101c79c07ff03d514d096cb0cf4b
Robert Marquez <legitem2023@gmail.com>
2025-06-30 09:38:10 +0800

    Update HomeTab.tsx

--- components/Home/HomeTab.tsx
+++ components/Home/HomeTab.tsx
@@ -1,27 +1,18 @@
 'use client'
-import Menu from 'components/Partial/Menu'
 import React from 'react'
-//import { ViewGallery } from 'components/Gallery/ViewGallery'
-// import { useGlobalState } from 'state'
-import { Icon } from '@iconify/react'
-import Carousel from 'components/Carousel'
-import Commercial3DModel from 'components/Partial/ThreeJS/Commercial3DModel'
-import ThreeJS from 'components/Partial/ThreeJS/ThreeJS'
-import Loading from 'components/Partial/LoadingAnimation/Loading'
-import { useQuery } from '@apollo/client'
-import { GET_CATEGORY } from 'graphql/queries'
 import { useSelector } from 'react-redux'
 import ReusableFlexLayout from 'components/Layout/ReusableFlexLayout'
 import Home from './Home'
-import Ads from 'components/Ads/Ads';
+import Ads from 'components/Ads/Ads'
 
-const HomeTab:React.FC = () => {
-const storeproductType = useSelector((state: any) => state.productTypeData.productTypeData);
+const HomeTab: React.FC = () => {
+  const storeproductType = useSelector((state: any) => state.productTypeData.productTypeData)
   return (
-     <ReusableFlexLayout 
-     childA={()=>(<Ads/>)} 
-     childB={()=>(<Home/>)}
-     childC={()=>(<Ads/>)}/>
+    <ReusableFlexLayout 
+      childA={() => (<Ads />)} 
+      childB={() => (<Home />)} 
+      childC={() => (<Ads />)} 
+    />
   )
 }
 
