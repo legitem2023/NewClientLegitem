d632a274496b70b55713a5992d8aebe83d5d5909
Robert Marquez <legitem2023@gmail.com>
2025-06-30 16:54:58 +0800

    Update ReusableFlexLayout.tsx

--- components/Layout/ReusableFlexLayout.tsx
+++ components/Layout/ReusableFlexLayout.tsx
@@ -8,14 +8,12 @@ const ReusableFlexLayout:FC<ReusableMainLayoutProps> = ({childA,childB,childC}) 
     return (
     <div className='FlexContainer'>
       <div className="LeftContainer">
-        <span style={{fontSize:'12px',padding:'5px'}}>Advertisement</span>
         {childA()}
       </div>
       <div className='CentralContainer' style={{ position:'relative' }}>
         {childB()}
       </div>
       <div className='RightContainer'>
-        <span style={{fontSize:'12px',padding:'5px'}}>Advertisement</span>
         {childC()}
       </div>
     </div>
