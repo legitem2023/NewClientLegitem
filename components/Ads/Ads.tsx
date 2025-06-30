cc07ea24630ba7c3c85f701e3d662589c16cb64f
Robert Marquez <legitem2023@gmail.com>
2025-06-30 18:14:58 +0800

    Update Ads.tsx

--- components/Ads/Ads.tsx
+++ components/Ads/Ads.tsx
@@ -4,7 +4,7 @@ import Marquee from 'components/UI/Marquee';
 import AdDropdown from './AdDropdown';
 const Ads:React.FC = () => {
 
-  const Dropdown = (<Marquee >Spotted! The perfect place for your products call now 09153392813</Marquee>)
+  const Dropdown = (<Marquee text="Spotted! The perfect place for your products call now 09153392813" fontSize="45px" speed={5} />)
   return (
      <AdDropdown content={Dropdown}/>
   )
