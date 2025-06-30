d276620600637f1e1bc5c21da5f786237d3e56fa
Robert Marquez <legitem2023@gmail.com>
2025-06-30 16:30:45 +0800

    Update Ads.tsx

--- components/Ads/Ads.tsx
+++ components/Ads/Ads.tsx
@@ -1,12 +1,16 @@
 'use client'
 import ReusableSlideNames from 'components/Reusable/ReusableSlideNames';
+import AdDropdown from './AdDropdown';
 const Ads:React.FC = () => {
 //const storeproductType = useSelector((state: any) => state.productTypeData.productTypeData);
   const Ads = [{ Name:'Spotted!'},
                { Name:'The perfect place for your product.' },
                { Name:'Call now: 09153392813'}]
+  
+  const Dropdown = (<ReusableSlideNames data={Ads}/>)
+  
   return (
-     <ReusableSlideNames data={Ads}/>
+     <AdDropdown content={Dropdown}/>
   )
 }
 
