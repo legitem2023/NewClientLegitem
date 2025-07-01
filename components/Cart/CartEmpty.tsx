f775aa5d310a6416332cf8107fdd9c7d14ecddb1
Robert Marquez <legitem2023@gmail.com>
2025-07-01 08:40:40 +0800

    Update CartEmpty.tsx

--- components/Cart/CartEmpty.tsx
+++ components/Cart/CartEmpty.tsx
@@ -3,7 +3,7 @@ const CartEmpty = () => {
   return (
     <div className='game-icons--shopping-cart'>
     <div>
-      <h2>Your cart is Empty</h2>
+      <p style={{fontWeight:'bold',fontSize:'30px'}}>Your cart is Empty</p>
       <p>Looks like you have not added anything to your cart Go ahead and explore the shop</p>
       <button className='universalButtonStyle'>Explore</button>
     </div>
